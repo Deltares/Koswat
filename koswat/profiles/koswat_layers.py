45ea86ac645102c8d288e54c10222e0a492e06d8
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-09-30 14:41:06 +0200

    feat: It is now possible to create layers from a dictionary

--- koswat/profiles/koswat_layers.py
+++ koswat/profiles/koswat_layers.py
@@ -1,13 +1,25 @@
-from typing import List, Optional
+from __future__ import annotations
 
+import math
+from typing import List
 
-class KoswatMaterial:
-    name: str
+from koswat.profiles.koswat_material import KoswatMaterial, KoswatMaterialFactory
 
 
 class KoswatLayer:
     material: KoswatMaterial
-    depth: Optional[float]
+    depth: float
+
+    def __init__(self) -> None:
+        self.material = None
+        self.depth = math.nan
+
+    @classmethod
+    def from_dict(cls, data_dict: dict) -> KoswatLayer:
+        _layer = cls()
+        _layer.material = KoswatMaterialFactory.get_material(data_dict["material"])
+        _layer.depth = data_dict.get("depth", math.nan)
+        return _layer
 
 
 class KoswatLayers:
@@ -17,3 +29,12 @@ class KoswatLayers:
     def __init__(self) -> None:
         self.base_layer = None
         self.coating_layers = []
+
+    @classmethod
+    def from_dict(cls, data_dict: dict) -> KoswatLayers:
+        _layers = KoswatLayers()
+        _layers.base_layer = KoswatLayer.from_dict(data_dict["base_layer"])
+        _layers.coating_layers = list(
+            map(KoswatLayer.from_dict, data_dict.get("coating_layers", []))
+        )
+        return _layers
