73d90a5efefe3e9017e19859b5e601de9f96a6d8
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-10-07 20:29:21 +0200

    test: Add coverage test

---
+++ tests/dike/surroundings/point/test_point_surroundings_builder.py
@@ -0,0 +1,12 @@
+from koswat.builder_protocol import BuilderProtocol
+from koswat.dike.surroundings.point.point_surroundings_builder import (
+    PointSurroundingsBuilder,
+)
+
+
+class TestPointSurroundingsBuilder:
+    def test_initialize(self):
+        _builder = PointSurroundingsBuilder()
+        assert isinstance(_builder, PointSurroundingsBuilder)
+        assert isinstance(_builder, BuilderProtocol)
+        assert not _builder.point_surroundings_data
