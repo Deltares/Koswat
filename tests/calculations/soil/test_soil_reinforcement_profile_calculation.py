a5d4bfa74dc05391fb963c95e62cc85a0937e802
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-10-10 19:00:46 +0200

    tests: Reduced code duplication by unifying tests in upper test class.

--- tests/calculations/soil/test_soil_reinforcement_profile_calculation.py
+++ tests/calculations/soil/test_soil_reinforcement_profile_calculation.py
@@ -9,15 +9,7 @@ from koswat.calculations.soil.soil_reinforcement_profile_calculation import (
     SoilReinforcementProfileCalculation,
 )
 from koswat.dike.profile.koswat_input_profile_base import KoswatInputProfileBase
-from koswat.dike.profile.koswat_profile_builder import KoswatProfileBuilder
 from koswat.koswat_scenario import KoswatScenario
-from tests.calculations import compare_koswat_profiles
-from tests.library_test_cases import (
-    InputProfileCases,
-    InputProfileScenarioLookup,
-    LayersCases,
-    ScenarioCases,
-)
 
 
 class TestSoilReinforcementProfileCalculation:
@@ -29,59 +21,6 @@ class TestSoilReinforcementProfileCalculation:
         assert isinstance(_calculation, SoilReinforcementProfileCalculation)
         assert isinstance(_calculation, ReinforcementProfileCalculationProtocol)
         assert isinstance(_calculation, BuilderProtocol)
-
-    @pytest.mark.parametrize(
-        "profile_data, scenario_data, expected_profile_data",
-        [
-            pytest.param(
-                InputProfileCases.default,
-                ScenarioCases.default,
-                InputProfileScenarioLookup.reinforcement_soil_default_default_no_layers,
-                id="Default input profile, Default Scenario",
-            ),
-            pytest.param(
-                InputProfileCases.default,
-                ScenarioCases.scenario_2,
-                InputProfileScenarioLookup.reinforcement_soil_default_scenario_2_no_layers,
-                id="Default input profile, Scenario 2",
-            ),
-        ],
-    )
-    def test_given_profile_and_scenario_calculate_new_geometry(
-        self,
-        profile_data: dict,
-        scenario_data: dict,
-        expected_profile_data: dict,
-    ):
-        # 1. Define test data.
-        _dummy_layers = LayersCases.without_layers
-        expected_profile_data["profile_type"] = SoilReinforcementProfile
-        _expected_profile = KoswatProfileBuilder.with_data(
-            expected_profile_data
-        ).build()
-        assert isinstance(_expected_profile, SoilReinforcementProfile)
-        _profile = KoswatProfileBuilder.with_data(
-            dict(
-                input_profile_data=profile_data,
-                layers_data=_dummy_layers,
-                p4_x_coordinate=0,
-                profile_type=SoilReinforcementProfile,
-            )
-        ).build()
-        assert isinstance(_profile, SoilReinforcementProfile)
-        _scenario = KoswatScenario.from_dict(dict(scenario_data))
-        assert isinstance(_scenario, KoswatScenario)
-
-        # 2. Run test.
-        _builder = SoilReinforcementProfileCalculation()
-        _builder.base_profile = _profile
-        _builder.scenario = _scenario
-        _new_profile = _builder.build()
-
-        # 3. Verify expectations.
-        assert isinstance(_new_profile, SoilReinforcementProfile)
-        assert isinstance(_new_profile.input_data, KoswatInputProfileBase)
-        compare_koswat_profiles(_new_profile, _expected_profile)
 
     def test_calculate_new_binnen_talud(self):
         # 1. Define test data.
--- tests/calculations/stability_wall/test_stability_wall_reinforcement_profile_calculation.py
+++ tests/calculations/stability_wall/test_stability_wall_reinforcement_profile_calculation.py
@@ -1,30 +1,9 @@
-import pytest
-
 from koswat.builder_protocol import BuilderProtocol
-from koswat.calculations.piping_wall.piping_wall_reinforcement_profile import (
-    PipingWallReinforcementProfile,
-)
 from koswat.calculations.reinforcement_profile_calculation_protocol import (
     ReinforcementProfileCalculationProtocol,
 )
-from koswat.calculations.stability_wall.stability_wall_reinforcement_profile import (
-    StabilityWallReinforcementProfile,
-)
 from koswat.calculations.stability_wall.stability_wall_reinforcement_profile_calculation import (
     StabilityWallReinforcementProfileCalculation,
-)
-from koswat.dike.koswat_input_profile_protocol import KoswatInputProfileProtocol
-from koswat.dike.profile.koswat_input_profile_base import KoswatInputProfileBase
-from koswat.dike.profile.koswat_profile import KoswatProfileBase
-from koswat.dike.profile.koswat_profile_builder import KoswatProfileBuilder
-from koswat.koswat_scenario import KoswatScenario
-from tests import plot_profiles
-from tests.calculations import compare_koswat_profiles
-from tests.library_test_cases import (
-    InputProfileCases,
-    InputProfileScenarioLookup,
-    LayersCases,
-    ScenarioCases,
 )
 
 
@@ -37,51 +16,3 @@ class TestStabilityWallReinforcementProfileCalculation:
         assert isinstance(_calculation, StabilityWallReinforcementProfileCalculation)
         assert isinstance(_calculation, ReinforcementProfileCalculationProtocol)
         assert isinstance(_calculation, BuilderProtocol)
-
-    @pytest.mark.parametrize(
-        "profile_data, scenario_data, expected_profile_data",
-        [
-            pytest.param(
-                InputProfileCases.default,
-                ScenarioCases.scenario_3,
-                InputProfileScenarioLookup.reinforcement_stability_wall_default_scenario_3_no_layers,
-                id="Default input profile, Scenario 3",
-            ),
-        ],
-    )
-    def test_given_profile_and_scenario_calculate_new_geometry(
-        self,
-        profile_data: dict,
-        scenario_data: dict,
-        expected_profile_data: dict,
-    ):
-        # 1. Define test data.
-        _dummy_layers = LayersCases.without_layers
-        expected_profile_data["profile_type"] = PipingWallReinforcementProfile
-        _expected_profile = KoswatProfileBuilder.with_data(
-            expected_profile_data
-        ).build()
-        assert isinstance(_expected_profile, PipingWallReinforcementProfile)
-        _base_profile = KoswatProfileBuilder.with_data(
-            dict(
-                input_profile_data=profile_data,
-                layers_data=_dummy_layers,
-                p4_x_coordinate=0,
-                profile_type=KoswatProfileBase,
-            )
-        ).build()
-        assert isinstance(_base_profile, KoswatProfileBase)
-        _scenario = KoswatScenario.from_dict(dict(scenario_data))
-        assert isinstance(_scenario, KoswatScenario)
-
-        # 2. Run test.
-        _builder = StabilityWallReinforcementProfileCalculation()
-        _builder.base_profile = _base_profile
-        _builder.scenario = _scenario
-        _new_profile = _builder.build()
-
-        # 3. Verify expectations.
-        assert isinstance(_new_profile, StabilityWallReinforcementProfile)
-        assert isinstance(_new_profile.input_data, KoswatInputProfileBase)
-        compare_koswat_profiles(_new_profile, _expected_profile)
-        plot_profiles(_base_profile, _new_profile)
