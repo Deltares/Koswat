858b7ce80589ba47b89c2fb3beac772d28d6ece9
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-10-10 22:21:57 +0200

    chore(CofferdamReinforcementProfileCalculation): Added calculations for CofferdamReinforcementProfile and tests

--- koswat/calculations/cofferdam/cofferdam_reinforcement_profile_calculation.py
+++ koswat/calculations/cofferdam/cofferdam_reinforcement_profile_calculation.py
@@ -20,24 +20,60 @@ class CofferdamReinforcementProfileCalculation(ReinforcementProfileCalculationPr
         self.base_profile = None
         self.scenario = None
 
+    def _calculate_new_kruin_hoogte(
+        self, base_data: KoswatInputProfileBase, scenario: KoswatScenario
+    ) -> float:
+        return base_data.kruin_hoogte + scenario.d_h
+
+    def _calculate_new_binnen_talud(
+        self, base_data: KoswatInputProfileBase, scenario: KoswatScenario
+    ) -> float:
+        """
+        ( Kruin_Breedte_Oud
+            + (Kruin_Hoogte_Oud-Binnen_Maaiveld_Oud)
+                *Binnen_Talud_Oud-Kruin_Breedte_Nieuw)
+        /(Kruin_Hoogte_Oud+dH)
+        """
+        _mid_operand = base_data.binnen_talud * (
+            base_data.kruin_hoogte - base_data.binnen_maaiveld
+        )
+        _operand = base_data.kruin_breedte + _mid_operand - scenario.kruin_breedte
+        _dividend = base_data.kruin_hoogte + scenario.d_h
+        return _operand / _dividend
+
+    def _calculate_new_buiten_talud(
+        self, base_data: KoswatInputProfileBase, scenario: KoswatScenario
+    ) -> float:
+        """
+        Kruin_Hoogte_Oud*Buiten_Talud_Oud
+        /(Kruin_Hoogte_Oud+dH)
+        """
+        _operand = base_data.kruin_hoogte * base_data.buiten_talud
+        _dividend = base_data.kruin_hoogte + scenario.d_h
+        return _operand / _dividend
+
+    def _calculate_new_length_coffer_dam(
+        self, base_data: KoswatInputProfileBase, scenario: KoswatScenario
+    ) -> float:
+        return (base_data.kruin_hoogte + scenario.d_h) - 1 + 10
+
     def _calculate_new_input_profile(
         self, base_data: KoswatInputProfileBase, scenario: KoswatScenario
     ) -> CofferDamInputProfile:
         _new_data = CofferDamInputProfile()
         _new_data.buiten_maaiveld = base_data.buiten_maaiveld
-        _new_data.buiten_talud = scenario.buiten_talud
+        _new_data.buiten_talud = self._calculate_new_buiten_talud(base_data, scenario)
         _new_data.buiten_berm_hoogte = base_data.buiten_berm_hoogte
         _new_data.buiten_berm_breedte = base_data.buiten_berm_breedte
         _new_data.kruin_hoogte = self._calculate_new_kruin_hoogte(base_data, scenario)
         _new_data.kruin_breedte = scenario.kruin_breedte
         _new_data.binnen_talud = self._calculate_new_binnen_talud(base_data, scenario)
-        _new_data.binnen_berm_breedte = self._calculate_new_binnen_berm_breedte(
-            base_data, _new_data, scenario
-        )
-        _new_data.binnen_berm_hoogte = self._calculate_new_binnen_berm_hoogte(
-            base_data, _new_data, scenario
-        )
+        _new_data.binnen_berm_breedte = 0
+        _new_data.binnen_berm_hoogte = 0
         _new_data.binnen_maaiveld = base_data.binnen_maaiveld
+        _new_data.length_coffer_dam = self._calculate_new_length_coffer_dam(
+            base_data, scenario
+        )
         return _new_data
 
     def build(self) -> CofferdamReinforcementProfile:
--- tests/calculations/cofferdam/test_cofferdam_reinforcement_profile_calculation.py
+++ tests/calculations/cofferdam/test_cofferdam_reinforcement_profile_calculation.py
@@ -1,10 +1,16 @@
+import pytest
+
 from koswat.builder_protocol import BuilderProtocol
+from koswat.calculations.cofferdam.cofferdam_input_profile import CofferDamInputProfile
 from koswat.calculations.cofferdam.cofferdam_reinforcement_profile_calculation import (
     CofferdamReinforcementProfileCalculation,
 )
 from koswat.calculations.reinforcement_profile_calculation_protocol import (
     ReinforcementProfileCalculationProtocol,
 )
+from koswat.dike.koswat_input_profile_protocol import KoswatInputProfileProtocol
+from koswat.dike.profile.koswat_input_profile_base import KoswatInputProfileBase
+from koswat.koswat_scenario import KoswatScenario
 
 
 class TestCofferDamReinforcementProfileCalculation:
@@ -16,3 +22,118 @@ class TestCofferDamReinforcementProfileCalculation:
         assert isinstance(_calculation, CofferdamReinforcementProfileCalculation)
         assert isinstance(_calculation, ReinforcementProfileCalculationProtocol)
         assert isinstance(_calculation, BuilderProtocol)
+
+    def test_calculate_length_coffer_dam(self):
+        class MockInputData(KoswatInputProfileProtocol):
+            kruin_hoogte: float
+
+        # 1. Define test data.
+        _calculator = CofferdamReinforcementProfileCalculation()
+        _input_data = MockInputData()
+        _input_data.kruin_hoogte = 6
+        _scenario = KoswatScenario()
+        _scenario.d_h = 2
+        _expected_result = 17
+
+        # 2. Run test.
+        _result = _calculator._calculate_new_length_coffer_dam(_input_data, _scenario)
+
+        # 3. Verify expectations
+        assert _result == _expected_result
+
+    def test_calculate_new_kruin_hoogte(self):
+        class MockInputData(KoswatInputProfileProtocol):
+            kruin_hoogte: float
+
+        # 1. Define test data.
+        _calculator = CofferdamReinforcementProfileCalculation()
+        _input_data = MockInputData()
+        _input_data.kruin_hoogte = 6
+        _scenario = KoswatScenario()
+        _scenario.d_h = 2
+        _expected_result = 8
+
+        # 2. Run test.
+        _result = _calculator._calculate_new_kruin_hoogte(_input_data, _scenario)
+
+        # 3. Verify expectations
+        assert _result == _expected_result
+
+    def test_calculate_new_buiten_talud(self):
+        class MockInputData(KoswatInputProfileProtocol):
+            kruin_hoogte: float
+            buiten_talud: float
+
+        # 1. Define test data.
+        _calculator = CofferdamReinforcementProfileCalculation()
+        _input_data = MockInputData()
+        _input_data.kruin_hoogte = 6
+        _input_data.buiten_talud = 3
+        _scenario = KoswatScenario()
+        _scenario.d_h = 2
+        _expected_result = 2.25
+
+        # 2. Run test.
+        _result = _calculator._calculate_new_buiten_talud(_input_data, _scenario)
+
+        # 3. Verify expectations
+        assert _result == _expected_result
+
+    def test_calculate_new_binnen_talud(self):
+        class MockInputData(KoswatInputProfileProtocol):
+            kruin_breedte: float
+            kruin_hoogte: float
+            binnen_maaiveld: float
+            binnen_talud: float
+
+        # 1. Define test data.
+        _expected_result = 2.25
+        _calculator = CofferdamReinforcementProfileCalculation()
+        _input_data = MockInputData()
+        _input_data.kruin_hoogte = 6
+        _input_data.binnen_maaiveld = 0
+        _input_data.binnen_talud = 3
+        _input_data.kruin_breedte = 5
+        _scenario = KoswatScenario()
+        _scenario.d_h = 2
+        _scenario.kruin_breedte = 5
+        _scenario.buiten_talud = 3
+
+        # 2. Run test.
+        _result = _calculator._calculate_new_binnen_talud(_input_data, _scenario)
+
+        # 3. Verify expectations
+        assert _result == pytest.approx(_expected_result, 0.001)
+
+    def test_calculate_new_input_profile(self):
+        class MockInputData(KoswatInputProfileProtocol):
+            kruin_hoogte: float
+            binnen_maaiveld: float
+            binnen_talud: float
+            kruin_breedte: float
+            buiten_berm_breedte: float
+            buiten_talud: float
+
+        # 1. Define test data.
+        _calculator = CofferdamReinforcementProfileCalculation()
+        _input_data = MockInputData()
+        _input_data.kruin_hoogte = 30
+        _input_data.binnen_maaiveld = 2.3
+        _input_data.binnen_talud = 4.5
+        _input_data.kruin_breedte = 5.6
+        _input_data.buiten_maaiveld = 6.7
+        _input_data.buiten_berm_hoogte = 7.8
+        _input_data.buiten_berm_breedte = 8.9
+        _input_data.buiten_talud = 9.9
+        _scenario = KoswatScenario()
+        _scenario.d_h = 12
+        _scenario.kruin_breedte = 6.7
+        _scenario.buiten_talud = 7.8
+
+        # 2. Run test.
+        _result = _calculator._calculate_new_input_profile(_input_data, _scenario)
+
+        # 3. Verify expectations
+        assert isinstance(_result, CofferDamInputProfile)
+        assert isinstance(_result, KoswatInputProfileBase)
+        assert isinstance(_result, KoswatInputProfileProtocol)
