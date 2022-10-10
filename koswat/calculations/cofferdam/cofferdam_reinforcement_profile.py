ec1e46eae049b0724786a924a61d025194462214
Carsopre <Carsopre@users.noreply.github.com>
2022-10-10 20:42:36 +0000

    autoformat: isort & black

--- koswat/calculations/cofferdam/cofferdam_reinforcement_profile.py
+++ koswat/calculations/cofferdam/cofferdam_reinforcement_profile.py
@@ -1,8 +1,8 @@
+from koswat.calculations.cofferdam.cofferdam_input_profile import CofferDamInputProfile
 from koswat.calculations.reinforcement_profile_protocol import (
     ReinforcementProfileProtocol,
 )
 from koswat.dike.profile.koswat_profile import KoswatProfileBase
-from koswat.calculations.cofferdam.cofferdam_input_profile import CofferDamInputProfile
 
 
 class CofferdamReinforcementProfile(ReinforcementProfileProtocol, KoswatProfileBase):
