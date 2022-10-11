8fed62b6c3a0a29dc9c63fbeb6674aa377c60b46
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-10-11 12:41:25 +0200

    chore(SummaryMatrixCsvExporter): Added logic to generate the matrix of costs and locations

--- koswat/cost_report/io/csv/summary_matrix_csv_fom.py
+++ koswat/cost_report/io/csv/summary_matrix_csv_fom.py
@@ -15,6 +15,12 @@ class SummaryMatrixCsvFom(FileObjectModelProtocol):
         self.location_rows = []
 
     def is_valid(self) -> bool:
-        return any(self.headers) and len(self.headers) == len(self.headers) == len(
-            self.location_rows
+        if not self.headers:
+            return False
+        _len_headers = len(self.headers)
+        _valid_cost_rows = all(len(_c_row) == _len_headers for _c_row in self.cost_rows)
+        _valid_loc_rows = all(
+            len(_l_row) == _len_headers for _l_row in self.location_rows
         )
+
+        return _valid_cost_rows and _valid_loc_rows
--- tests/cost_report/io/csv/test_summary_matrix_csv_exporter.py
+++ tests/cost_report/io/csv/test_summary_matrix_csv_exporter.py
@@ -1,4 +1,7 @@
-from typing import Type
+import random
+from typing import List, Type
+
+from shapely.geometry import Point
 
 from koswat.calculations import (
     CofferdamReinforcementProfile,
@@ -11,17 +14,25 @@ from koswat.cost_report.io.csv.summary_matrix_csv_exporter import (
     SummaryMatrixCsvExporter,
 )
 from koswat.cost_report.io.csv.summary_matrix_csv_fom import SummaryMatrixCsvFom
+from koswat.cost_report.layer.layer_cost_report import LayerCostReport
 from koswat.cost_report.multi_location_profile.multi_location_profile_cost_report import (
     MultiLocationProfileCostReport,
 )
+from koswat.cost_report.profile.profile_cost_report import ProfileCostReport
 from koswat.cost_report.summary.koswat_summary import KoswatSummary
+from koswat.dike.surroundings.point.point_surroundings import PointSurroundings
 from koswat.io.koswat_exporter_protocol import KoswatExporterProtocol
 
 
 class MockSummary(MultiLocationProfileCostReport):
     total_cost = 42000
-    total_volume = 24000
     profile_type: str = ""
+    cost_per_km = 42
+
+
+class MockLayerReport(LayerCostReport):
+    material = ""
+    total_volume = 42
 
 
 class TestSummaryMatrixCsvExporter:
@@ -31,12 +42,40 @@ class TestSummaryMatrixCsvExporter:
         assert isinstance(_exporter, KoswatExporterProtocol)
         assert not _exporter.data_object_model
 
+    def _create_locations(self, locations: int) -> List[PointSurroundings]:
+        _points = []
+        for _ in range(0, locations):
+            _x_coord = random.uniform(0, 4).__round__(2)
+            _y_coord = random.uniform(0, 4).__round__(2)
+            _ps = PointSurroundings()
+            _ps.location = Point(_x_coord, _y_coord)
+            _ps.section = "A"
+            _points.append(_ps)
+        return _points
+
     def _create_report(
-        self, report_type: Type[ReinforcementProfileProtocol]
+        self,
+        report_type: Type[ReinforcementProfileProtocol],
+        available_points: List[PointSurroundings],
     ) -> MultiLocationProfileCostReport:
+        def _get_layer(material: str, volume: float) -> MockLayerReport:
+            _layer_report = MockLayerReport()
+            _layer_report.material = material
+            _layer_report.total_volume = volume
+            return _layer_report
+
         _report = MockSummary()
+        _sel_locations = random.choice(range(0, len(available_points)))
+        _report.locations = random.sample(available_points, k=_sel_locations)
+        _required_klei = random.uniform(1, 100).__round__(2) * _sel_locations
+        _required_zand = random.uniform(1, 100).__round__(2) * _sel_locations
         _report.profile_type = str(report_type())
-        _report.locations = []
+        _report.cost_per_km = (_required_klei + _required_zand) * 1234
+        _report.profile_cost_report = ProfileCostReport()
+        _report.profile_cost_report.layer_cost_reports = [
+            _get_layer("Klei", _required_klei),
+            _get_layer("Zand", _required_zand),
+        ]
         return _report
 
     def test_build(self):
@@ -50,8 +89,9 @@ class TestSummaryMatrixCsvExporter:
             SoilReinforcementProfile,
             StabilityWallReinforcementProfile,
         ]
+        _available_points = self._create_locations(len(_required_profiles))
         _summary.locations_profile_report_list = list(
-            map(self._create_report, _required_profiles)
+            map(lambda x: self._create_report(x, _available_points), _required_profiles)
         )
 
         # 2. Run test
