cb07426e654cbb73b8bdceaecc70b9b4b0ceb026
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-11-11 15:38:48 +0100

    chore: Added missing files from previous commit

---
+++ koswat/dike/layers/koswat_layers_wrapper_builder_protocol.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import List, Protocol
+
+from shapely import geometry
+
+from koswat.builder_protocol import BuilderProtocol
+from koswat.dike.layers.koswat_layers_wrapper import KoswatLayersWrapperProtocol
+
+
+class KoswatLayersWrapperBuilderProtocol(BuilderProtocol, Protocol):
+    layers_data: dict
+    profile_points: List[geometry.Point]
+
+    def build(self) -> KoswatLayersWrapperProtocol:
+        pass
