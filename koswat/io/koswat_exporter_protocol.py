9500b79c5d26308c241d5bbc2e2d2d6c766aa057
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-11-18 23:27:22 +0100

    chore: Run black

--- koswat/io/koswat_exporter_protocol.py
+++ koswat/io/koswat_exporter_protocol.py
@@ -5,7 +5,6 @@ from typing_extensions import runtime_checkable
 
 @runtime_checkable
 class KoswatExporterProtocol(Protocol):
-
     def export(self, **kwargs) -> None:
         """
         Exports an object model into a concrete file format.
