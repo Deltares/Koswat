f696c8190cc2c4345d0503d22649c192fd325eb5
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-11-21 19:45:59 +0100

    test(test_profile_cost_report): Fixed failing test

--- tests/cost_report/profile/test_profile_cost_report.py
+++ tests/cost_report/profile/test_profile_cost_report.py
@@ -1,6 +1,7 @@
 import math
 
 from koswat.cost_report.profile.profile_cost_report import ProfileCostReport
+from koswat.cost_report.profile.volume_cost_parameters import VolumeCostParameters
 
 
 class TestProfileCostReport:
@@ -8,7 +9,7 @@ class TestProfileCostReport:
         _report = ProfileCostReport()
         assert isinstance(_report, ProfileCostReport)
         assert not _report.reinforced_profile
-        assert not _report.volume_cost_parameters
+        assert isinstance(_report.volume_cost_parameters, VolumeCostParameters)
         assert not any(_report.layer_cost_reports)
-        assert math.isnan(_report.total_cost)
-        assert math.isnan(_report.total_volume)
+        assert _report.total_cost == 0
+        assert _report.total_volume == 0
