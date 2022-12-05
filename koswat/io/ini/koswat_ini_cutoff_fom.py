1b80ccbc11e6e963ed6030e1a6872451c7a7a513
levelt <david.levelt@deltares.nl>
2022-12-05 15:39:44 +0100

    Issue 41 Language issues?

---
+++ koswat/io/ini/koswat_ini_cutoff_fom.py
@@ -0,0 +1,32 @@
+import math
+
+from koswat.io.koswat_reader_protocol import FileObjectModelProtocol
+
+
+class KoswatIniGroundMeasuresFom(FileObjectModelProtocol):
+    storageFactorGround: str
+    storageFactorConstruction: str
+    storageFactorGroundPurchase: str
+    minLenghtCutoff: float
+    transitionCBWallSheetPile: int
+    maxLenghtCutoff: float
+
+    def __init__(self) -> None:
+        self.storageFactorGround = ""
+        self.storageFactorConstruction = ""
+        self.storageFactorGroundPurchase = ""
+        self.minLenghtCutoff = math.nan
+        self.transitionCBWallSheetPile = math.nan
+        self.maxLenghtCutoff = math.nan
+
+    def is_valid(self) -> bool:
+        # TODO add validation
+        #        check if values are initialized
+        # how to test for an uninitialized bool?
+        return (
+            (self.storageFactorGround != "")
+            & (self.storageFactorGroundPurchase != "")
+            & (self.minLenghtCutoff != math.nan)
+            & (self.transitionCBWallSheetPile != math.nan)
+            & (self.maxLenghtCutoff != math.nan)
+        )
