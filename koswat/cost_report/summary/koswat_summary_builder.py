0fa68f53a6da1e3f937a3cc2b9a1cfd580395b9d
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-12-19 23:02:34 +0100

    chore: Further removal of try-except-raise to allow plot generation

--- koswat/cost_report/summary/koswat_summary_builder.py
+++ koswat/cost_report/summary/koswat_summary_builder.py
@@ -34,11 +34,12 @@ class KoswatSummaryBuilder(BuilderProtocol):
                 _calc_profile = _builder.build()
                 _calculated_profiles.append(_calc_profile)
             except Exception as e_info:
+                # TODO: This should never happen.
                 logging.error(
                     "Error calculating reinforcement: {}".format(_reinforcement_type())
                 )
                 logging.error(e_info)
-                raise
+
         return _calculated_profiles
 
     def _get_multi_location_profile_cost_builder(
@@ -50,6 +51,13 @@ class KoswatSummaryBuilder(BuilderProtocol):
 
     def build(self) -> KoswatSummary:
         _summary = KoswatSummary()
+        logging.info(
+            "Creating analysis for {} - scenario {} - {}".format(
+                self.run_scenario_settings.input_profile_case.input_data.dike_section,
+                self.run_scenario_settings.scenario.scenario_section,
+                self.run_scenario_settings.scenario.scenario_name,
+            )
+        )
         _mlpc_builder = self._get_multi_location_profile_cost_builder()
         for _calc_profile in self._get_calculated_profile_list():
             _mlpc_builder.reinforced_profile = _calc_profile
--- koswat/koswat_handler.py
+++ koswat/koswat_handler.py
@@ -14,44 +14,49 @@ from koswat.cost_report.io.csv.summary_matrix_csv_exporter import (
 from koswat.cost_report.io.plots.multi_location_profile_comparison_plot_exporter import (
     MultiLocationProfileComparisonPlotExporter,
 )
+from koswat.cost_report.summary.koswat_summary import KoswatSummary
 from koswat.cost_report.summary.koswat_summary_builder import KoswatSummaryBuilder
 from koswat.koswat_logger import KoswatLogger
 
 
 class KoswatHandler:
-    def _generate_summary(self, _run_scenario: KoswatRunScenarioSettings) -> None:
+    def _generate_summary(
+        self, run_scenario: KoswatRunScenarioSettings
+    ) -> KoswatSummary:
         _summary_builder = KoswatSummaryBuilder()
-        _summary_builder.run_scenario_settings = _run_scenario
-        logging.info(
-            "Creating analysis for {} - scenario {} - {}".format(
-                _run_scenario.input_profile_case.input_data.dike_section,
-                _run_scenario.scenario.scenario_section,
-                _run_scenario.scenario.scenario_name,
-            )
-        )
+        _summary_builder.run_scenario_settings = run_scenario
         _summary = _summary_builder.build()
         logging.info("Analysis created.")
+        return _summary
+
+    def _export_summary(
+        self, settings: KoswatRunScenarioSettings, summary: KoswatSummary
+    ) -> None:
         # Export analysis results (csv and plots)
-        _run_scenario.output_dir.mkdir(parents=True, exist_ok=True)
-        logging.info("Exporting csv results to {}.".format(_run_scenario.output_dir))
-        if not _summary.locations_profile_report_list:
-            logging.error("No summary was genarted for {}".format(_run_scenario.name))
+        settings.output_dir.mkdir(parents=True, exist_ok=True)
+        logging.info("Exporting csv results to {}.".format(settings.output_dir))
+        if not summary.locations_profile_report_list:
+            logging.error("No summary was genarted for {}".format(settings.name))
             return
         # Export analysis csv.
         _exporter = SummaryMatrixCsvExporter()
-        _exporter.data_object_model = _summary
-        _exporter.export_filepath = _run_scenario.output_dir / "matrix_results.csv"
+        _exporter.data_object_model = summary
+        _exporter.export_filepath = settings.output_dir / "matrix_results.csv"
         _exporter.export(_exporter.build())
         logging.info("Exported matrix results to: {}".format(_exporter.export_filepath))
+
+    def _generate_plots(
+        self, settings: KoswatRunScenarioSettings, summary: KoswatSummary
+    ) -> None:
         # Export analysis plots
-        for _multi_report in _summary.locations_profile_report_list:
+        for _multi_report in summary.locations_profile_report_list:
             try:
                 _mlp_plot = MultiLocationProfileComparisonPlotExporter()
                 _mlp_plot.cost_report = _multi_report
-                _mlp_plot.export_dir = _run_scenario.output_dir
+                _mlp_plot.export_dir = settings.output_dir
                 _mlp_plot.export()
                 logging.info(
-                    "Exported comparison plots to: {}".format(_run_scenario.output_dir)
+                    "Exported comparison plots to: {}".format(settings.output_dir)
                 )
             except Exception as e_info:
                 logging.error(
@@ -77,13 +82,17 @@ class KoswatHandler:
         # Run data
         for _run_scenario in self._run_settings.run_scenarios:
             try:
-                self._generate_summary(_run_scenario)
+                _summary = self._generate_summary(_run_scenario)
+                self._export_summary(_run_scenario, _summary)
+                self._generate_plots(_run_scenario, _summary)
             except Exception as e_info:
                 logging.error(
                     "Error while running scenario {}, more details: {}".format(
                         _run_scenario.name, e_info
                     )
                 )
+                if __debug__:
+                    raise
 
     def __enter__(self) -> KoswatHandler:
         self._logger = KoswatLogger.init_logger(Path("koswat.log"))
