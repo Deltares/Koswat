f63cd22dd1bbd79f3cd0c9872205012c6c1d723e
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-12-20 15:35:48 +0100

    fix(koswat/__main__.py): Renamed main file to fit to python main expected module

--- koswat/__init__.py
+++ koswat/__init__.py
@@ -1,11 +1,1 @@
 __version__ = "0.11.0"
-
-import logging
-
-import koswat.main
-
-# if __name__ == "__main__":
-#     try:
-#         run_analysis()
-#     except Exception as e_info:
-#         logging.error(str(e_info))
