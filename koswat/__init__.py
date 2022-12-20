8b87308af011f62a2612cb5529cd74782314fcae
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-12-20 15:12:11 +0100

    ci(koswat): moved main logic to koswat __init__

--- koswat/__init__.py
+++ koswat/__init__.py
@@ -1,3 +1,11 @@
 __version__ = "0.11.0"
 
-import koswat.main
+import logging
+
+from koswat.main import run_analysis
+
+if __name__ == "__main__":
+    try:
+        run_analysis()
+    except Exception as e_info:
+        logging.error(str(e_info))
