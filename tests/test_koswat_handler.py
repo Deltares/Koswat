003182cb9109e5cab7946a28e976b39d71570bb2
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-12-20 13:44:13 +0100

    chore: Fixed (silly) code smell from sonarcloud.

--- tests/test_koswat_handler.py
+++ tests/test_koswat_handler.py
@@ -5,6 +5,8 @@ import pytest
 from koswat.koswat_handler import KoswatHandler
 from tests import get_test_results_dir, test_data
 
+log_filter = "*.log"
+
 
 class TestKoswatHandler:
     def test_koswat_handler_initialize(self):
@@ -15,17 +17,17 @@ class TestKoswatHandler:
         self, request: pytest.FixtureRequest
     ):
         _results = get_test_results_dir(request)
-        assert not any(_results.glob("*.log"))
+        assert not any(_results.glob(log_filter))
         with KoswatHandler(_results) as _handler:
             assert isinstance(_handler, KoswatHandler)
-            assert any(_results.glob("*.log"))
+            assert any(_results.glob(log_filter))
 
     def test_koswat_handler_run_analysis_given_valid_data(
         self, request: pytest.FixtureRequest
     ):
         # 1. Define test data.
         _results_dir = get_test_results_dir(request)
-        assert not any(_results_dir.glob("*.log"))
+        assert not any(_results_dir.glob(log_filter))
         _ini_file = test_data / "acceptance" / "koswat_general.ini"
         assert _ini_file.is_file()
 
@@ -34,7 +36,7 @@ class TestKoswatHandler:
             _handler.run_analysis(_ini_file)
 
         # 3. Verify results.
-        _log: Path = next(_results_dir.glob("*.log"), None)
+        _log: Path = next(_results_dir.glob(log_filter), None)
         assert _log and _log.is_file(), "Log file was not generated."
         assert (
             _log.read_text().find("ERROR") == -1
