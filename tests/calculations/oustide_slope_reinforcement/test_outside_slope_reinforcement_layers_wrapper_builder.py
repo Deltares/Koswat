1c37f4dac592ea0b6c6b467a76988b21388006e4
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2022-12-22 15:34:08 +0100

    test(outside_slope_reinforcement): Extended tests

---
+++ tests/calculations/oustide_slope_reinforcement/test_outside_slope_reinforcement_layers_wrapper_builder.py
@@ -0,0 +1,15 @@
+import pytest
+
+from koswat.calculations.outside_slope_reinforcement.outside_slope_reinforcement_layers_wrapper_builder import (
+    OutsideSlopeReinforcementLayersWrapperBuilder,
+)
+from koswat.dike.layers.layers_wrapper.koswat_layers_wrapper_builder_protocol import (
+    KoswatLayersWrapperBuilderProtocol,
+)
+
+
+class TestOutsideSlopeReinforcementLayersWrapperBuilder:
+    def test_initialize(self):
+        _builder = OutsideSlopeReinforcementLayersWrapperBuilder()
+        assert isinstance(_builder, OutsideSlopeReinforcementLayersWrapperBuilder)
+        assert isinstance(_builder, KoswatLayersWrapperBuilderProtocol)
