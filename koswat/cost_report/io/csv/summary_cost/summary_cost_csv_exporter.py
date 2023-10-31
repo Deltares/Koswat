3e71226a8afc3061e6a28b5a29aa59ed28f7bb1d
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-10-31 13:37:59 +0100

    chore: Added new exporter only for summary costs

---
+++ koswat/cost_report/io/csv/summary_cost/summary_cost_csv_exporter.py
@@ -0,0 +1,29 @@
+import logging
+from pathlib import Path
+from koswat.core.io.csv.koswat_csv_writer import KoswatCsvWriter
+from koswat.core.io.koswat_exporter_protocol import KoswatExporterProtocol
+from koswat.cost_report.io.csv.summary_cost.summary_cost_csv_fom_builder import (
+    SummaryCostCsvFomBuilder,
+)
+from koswat.cost_report.summary.koswat_summary import KoswatSummary
+
+
+class SummaryCostCsvExporter(KoswatExporterProtocol):
+    def export(self, koswat_summary: KoswatSummary, export_path: Path) -> None:
+        if not isinstance(koswat_summary, KoswatSummary):
+            raise ValueError("No 'KoswatSummary' object provided.")
+        if not isinstance(export_path, Path):
+            raise ValueError("No export path location provided.")
+
+        _csv_fom_builder = SummaryCostCsvFomBuilder()
+        _csv_fom_builder.koswat_summary = koswat_summary
+        _csv_fom = _csv_fom_builder.build()
+        if not _csv_fom:
+            logging.error(
+                "Export of KoswatSummary failed, no FileObjectModel was generated."
+            )
+            return
+
+        if not export_path.parent.is_dir():
+            export_path.parent.mkdir(parents=True)
+        KoswatCsvWriter().write(_csv_fom, export_path)
--- koswat/cost_report/io/csv/summary_matrix_csv_exporter.py
+++ koswat/cost_report/io/csv/summary_matrix_csv_exporter.py
@@ -1,10 +1,8 @@
-import logging
 from pathlib import Path
 
-from koswat.core.io.csv.koswat_csv_writer import KoswatCsvWriter
 from koswat.core.io.koswat_exporter_protocol import KoswatExporterProtocol
-from koswat.cost_report.io.csv.summary_matrix_csv_fom_builder import (
-    SummaryMatrixCsvFomBuilder,
+from koswat.cost_report.io.csv.summary_cost.summary_cost_csv_exporter import (
+    SummaryCostCsvExporter,
 )
 from koswat.cost_report.summary.koswat_summary import KoswatSummary
 
@@ -13,20 +11,4 @@ class SummaryMatrixCsvExporter(KoswatExporterProtocol):
     def export(
         self, koswat_summary: KoswatSummary, export_path: Path, **kwargs
     ) -> None:
-        if not isinstance(koswat_summary, KoswatSummary):
-            raise ValueError("No 'KoswatSummary' object provided.")
-        if not isinstance(export_path, Path):
-            raise ValueError("No export path location provided.")
-
-        _csv_fom_builder = SummaryMatrixCsvFomBuilder()
-        _csv_fom_builder.koswat_summary = koswat_summary
-        _csv_fom = _csv_fom_builder.build()
-        if not _csv_fom:
-            logging.error(
-                "Export of KoswatSummary failed, no FileObjectModel was generated."
-            )
-            return
-
-        if not export_path.parent.is_dir():
-            export_path.parent.mkdir(parents=True)
-        KoswatCsvWriter().write(_csv_fom, export_path)
+        SummaryCostCsvExporter().export(koswat_summary, export_path)
