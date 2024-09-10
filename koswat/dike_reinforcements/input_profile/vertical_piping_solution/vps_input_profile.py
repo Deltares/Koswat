354adcba87f00f973a52a6952bc20923559d047c
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-09-10 15:12:08 +0200

    chore: add inheritance

--- koswat/dike_reinforcements/input_profile/vertical_piping_solution/vps_input_profile.py
+++ koswat/dike_reinforcements/input_profile/vertical_piping_solution/vps_input_profile.py
@@ -1,23 +1,15 @@
 from dataclasses import dataclass
 
-from koswat.configuration.settings.koswat_general_settings import (
-    ConstructionTypeEnum,
-    SurtaxFactorEnum,
-)
-from koswat.dike.profile.koswat_input_profile_base import KoswatInputProfileBase
 from koswat.dike_reinforcements.input_profile.reinforcement_input_profile_protocol import (
     ReinforcementInputProfileProtocol,
+)
+from koswat.dike_reinforcements.input_profile.soil.soil_input_profile import (
+    SoilInputProfile,
 )
 
 
 @dataclass
-class VPSInputProfile(KoswatInputProfileBase, ReinforcementInputProfileProtocol):
-    construction_length: float = 0
-    construction_type: ConstructionTypeEnum | None = None
-    soil_surtax_factor: SurtaxFactorEnum = SurtaxFactorEnum.NORMAAL
-    constructive_surtax_factor: SurtaxFactorEnum | None = None
-    land_purchase_surtax_factor: SurtaxFactorEnum | None = None
-
+class VPSInputProfile(SoilInputProfile, ReinforcementInputProfileProtocol):
     @property
     def reinforcement_domain_name(self) -> str:
         return "Verticale piping oplossing"
--- koswat/dike_reinforcements/input_profile/vertical_piping_solution/vps_input_profile_calculation.py
+++ koswat/dike_reinforcements/input_profile/vertical_piping_solution/vps_input_profile_calculation.py
@@ -7,11 +7,11 @@ from koswat.configuration.settings.reinforcements.koswat_vps_settings import (
 )
 from koswat.dike.koswat_profile_protocol import KoswatProfileProtocol
 from koswat.dike.profile.koswat_input_profile_base import KoswatInputProfileBase
-from koswat.dike_reinforcements.input_profile.reinforcement_input_profile_calculation_base import (
-    ReinforcementInputProfileCalculationBase,
-)
 from koswat.dike_reinforcements.input_profile.reinforcement_input_profile_calculation_protocol import (
     ReinforcementInputProfileCalculationProtocol,
+)
+from koswat.dike_reinforcements.input_profile.soil.soil_input_profile_calculation import (
+    SoilInputProfileCalculation,
 )
 from koswat.dike_reinforcements.input_profile.vertical_piping_solution.vps_input_profile import (
     VPSInputProfile,
@@ -19,7 +19,7 @@ from koswat.dike_reinforcements.input_profile.vertical_piping_solution.vps_input
 
 
 class VPSInputProfileCalculation(
-    ReinforcementInputProfileCalculationBase,
+    SoilInputProfileCalculation,
     ReinforcementInputProfileCalculationProtocol,
 ):
     base_profile: KoswatProfileProtocol
@@ -27,67 +27,12 @@ class VPSInputProfileCalculation(
     scenario: KoswatScenario
 
     def __init__(self) -> None:
-        self.base_profile = None
-        self.scenario = None
-
-    def _calculate_new_binnen_talud(
-        self, base_data: KoswatInputProfileBase, scenario: KoswatScenario
-    ) -> float:
-        """
-        MAX(
-            Binnen_Talud_Oud,
-            (
-                dS
-                -dH*Buiten_Talud_Nieuw
-                -(Kruin_Breedte_Nieuw-Kruin_Breedte_Oud)
-                +(Kruin_Hoogte_Oud-Binnen_Maaiveld_Oud)*Binnen_Talud_Oud)
-                /(Kruin_Hoogte_Oud-Binnen_Maaiveld_Oud+dH))
-        """
-        _first_part = scenario.d_h * scenario.buiten_talud
-        _second_part = scenario.kruin_breedte - base_data.kruin_breedte
-        _third_parth = (
-            base_data.kruin_hoogte - base_data.binnen_maaiveld
-        ) * base_data.binnen_talud
-        _dividend = base_data.kruin_hoogte - base_data.binnen_maaiveld + scenario.d_h
-        _right_side = (
-            scenario.d_s - _first_part - _second_part + _third_parth
-        ) / _dividend
-        return max(base_data.binnen_talud, _right_side)
-
-    def _calculate_new_binnen_berm_hoogte(
-        self,
-        old_data: KoswatInputProfileBase,
-        new_data: KoswatInputProfileBase,
-        scenario: KoswatScenario,
-    ) -> float:
-        if new_data.binnen_berm_breedte > 0:
-            _max = max(
-                0.5,
-                (old_data.binnen_berm_hoogte - old_data.binnen_maaiveld),
-                new_data.binnen_berm_breedte * 0.05,
-            )
-            return (
-                min(
-                    _max,
-                    0.4
-                    * (
-                        (old_data.kruin_hoogte - old_data.binnen_maaiveld)
-                        + scenario.d_h
-                    ),
-                )
-                + old_data.binnen_maaiveld
-            )
-        return old_data.binnen_maaiveld
-
-    def _calculate_new_kruin_hoogte(
-        self, base_data: KoswatInputProfileBase, scenario: KoswatScenario
-    ) -> float:
-        return base_data.kruin_hoogte + scenario.d_h
+        super().__init__()
 
     def _calculate_new_input_profile(
         self,
         base_data: KoswatInputProfileBase,
-        soil_settings: KoswatVPSSettings,
+        vps_settings: KoswatVPSSettings,
         scenario: KoswatScenario,
     ) -> KoswatInputProfileBase:
         _new_data = VPSInputProfile()
@@ -100,9 +45,7 @@ class VPSInputProfileCalculation(
         _new_data.kruin_hoogte = self._calculate_new_kruin_hoogte(base_data, scenario)
         _new_data.binnen_maaiveld = base_data.binnen_maaiveld
         _new_data.binnen_talud = self._calculate_new_binnen_talud(base_data, scenario)
-        _new_data.binnen_berm_breedte = self._calculate_soil_binnen_berm_breedte(
-            base_data, _new_data, scenario
-        )
+        _new_data.binnen_berm_breedte = vps_settings.binnen_berm_breedte_vps
         _new_data.binnen_berm_hoogte = self._calculate_new_binnen_berm_hoogte(
             base_data, _new_data, scenario
         )
@@ -111,11 +54,9 @@ class VPSInputProfileCalculation(
         _new_data.factor_zetting = base_data.factor_zetting
         _new_data.pleistoceen = base_data.pleistoceen
         _new_data.aquifer = base_data.aquifer
-        _new_data.soil_surtax_factor = soil_settings.soil_surtax_factor
+        _new_data.soil_surtax_factor = vps_settings.soil_surtax_factor
         _new_data.constructive_surtax_factor = None
-        _new_data.land_purchase_surtax_factor = (
-            soil_settings.land_purchase_surtax_factor
-        )
+        _new_data.land_purchase_surtax_factor = vps_settings.land_purchase_surtax_factor
         return _new_data
 
     def build(self) -> VPSInputProfile:
