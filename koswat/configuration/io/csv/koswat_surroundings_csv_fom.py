af36b986acf526a87076aab8b44159e51864030c
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2024-09-11 10:11:43 +0200

    chore: Extracted fom class into its own file

--- koswat/configuration/io/csv/koswat_surroundings_csv_fom.py
+++ koswat/configuration/io/csv/koswat_surroundings_csv_fom.py
@@ -1,62 +1,34 @@
+from dataclasses import dataclass
+
+from koswat.configuration.io.csv.koswat_traject_surroundings_csv_fom import (
+    KoswatTrajectSurroundingsCsvFom,
+)
 from koswat.core.io.csv.koswat_csv_fom_protocol import KoswatCsvFomProtocol
-from koswat.dike.surroundings.point.point_surroundings import PointSurroundings
 
 
-class KoswatTrajectSurroundingsCsvFom(KoswatCsvFomProtocol):
-    points_surroundings_list: list[PointSurroundings]
-    distances_list: list[float]
-    traject: str
-
-    def __init__(self) -> None:
-        self.points_surroundings_list = []
-        self.distances_list = []
-        self.traject = ""
-
-    def is_valid(self) -> bool:
-        return self.points_surroundings_list and any(self.points_surroundings_list)
-
-
+@dataclass
 class KoswatTrajectSurroundingsWrapperCsvFom(KoswatCsvFomProtocol):
-    traject: str
-    buildings_polderside: KoswatTrajectSurroundingsCsvFom
-    buildings_dikeside: KoswatTrajectSurroundingsCsvFom
+    traject: str = ""
+    buildings_polderside: KoswatTrajectSurroundingsCsvFom = None
+    buildings_dikeside: KoswatTrajectSurroundingsCsvFom = None
 
-    railways_polderside: KoswatTrajectSurroundingsCsvFom
-    railways_dikeside: KoswatTrajectSurroundingsCsvFom
+    railways_polderside: KoswatTrajectSurroundingsCsvFom = None
+    railways_dikeside: KoswatTrajectSurroundingsCsvFom = None
 
-    waters_polderside: KoswatTrajectSurroundingsCsvFom
-    waters_dikeside: KoswatTrajectSurroundingsCsvFom
+    waters_polderside: KoswatTrajectSurroundingsCsvFom = None
+    waters_dikeside: KoswatTrajectSurroundingsCsvFom = None
 
-    roads_class_2_polderside: KoswatTrajectSurroundingsCsvFom
-    roads_class_7_polderside: KoswatTrajectSurroundingsCsvFom
-    roads_class_24_polderside: KoswatTrajectSurroundingsCsvFom
-    roads_class_47_polderside: KoswatTrajectSurroundingsCsvFom
-    roads_class_unknown_polderside: KoswatTrajectSurroundingsCsvFom
+    roads_class_2_polderside: KoswatTrajectSurroundingsCsvFom = None
+    roads_class_7_polderside: KoswatTrajectSurroundingsCsvFom = None
+    roads_class_24_polderside: KoswatTrajectSurroundingsCsvFom = None
+    roads_class_47_polderside: KoswatTrajectSurroundingsCsvFom = None
+    roads_class_unknown_polderside: KoswatTrajectSurroundingsCsvFom = None
 
-    roads_class_2_dikeside: KoswatTrajectSurroundingsCsvFom
-    roads_class_7_dikeside: KoswatTrajectSurroundingsCsvFom
-    roads_class_24_dikeside: KoswatTrajectSurroundingsCsvFom
-    roads_class_47_dikeside: KoswatTrajectSurroundingsCsvFom
-    roads_class_unknown_dikeside: KoswatTrajectSurroundingsCsvFom
-
-    def __init__(self) -> None:
-        self.traject = ""
-        self.buildings_polderside = None
-        self.buildings_dikeside = None
-        self.railways_polderside = None
-        self.railways_dikeside = None
-        self.waters_polderside = None
-        self.waters_dikeside = None
-        self.roads_class_2_polderside = None
-        self.roads_class_7_polderside = None
-        self.roads_class_24_polderside = None
-        self.roads_class_47_polderside = None
-        self.roads_class_unknown_polderside = None
-        self.roads_class_2_dikeside = None
-        self.roads_class_7_dikeside = None
-        self.roads_class_24_dikeside = None
-        self.roads_class_47_dikeside = None
-        self.roads_class_unknown_dikeside = None
+    roads_class_2_dikeside: KoswatTrajectSurroundingsCsvFom = None
+    roads_class_7_dikeside: KoswatTrajectSurroundingsCsvFom = None
+    roads_class_24_dikeside: KoswatTrajectSurroundingsCsvFom = None
+    roads_class_47_dikeside: KoswatTrajectSurroundingsCsvFom = None
+    roads_class_unknown_dikeside: KoswatTrajectSurroundingsCsvFom = None
 
     def is_valid(self) -> bool:
         _surroundings = [
