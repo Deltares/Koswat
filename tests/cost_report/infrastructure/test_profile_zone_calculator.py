d109f07a85e3cb7f46968495119ff35ad5845213
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2024-09-17 11:35:43 +0200

    test: Added (real) test cses for `ProfileZoneCalculator`

--- tests/cost_report/infrastructure/test_profile_zone_calculator.py
+++ tests/cost_report/infrastructure/test_profile_zone_calculator.py
@@ -10,9 +10,6 @@ from koswat.cost_report.infrastructure.profile_zone_calculator import (
     ProfileZoneCalculator,
 )
 from koswat.dike.characteristic_points.characteristic_points import CharacteristicPoints
-from koswat.dike.characteristic_points.characteristic_points_builder import (
-    CharacteristicPointsBuilder,
-)
 from koswat.dike.koswat_profile_protocol import KoswatProfileProtocol
 from koswat.dike.profile.koswat_profile import KoswatProfileBase
 from koswat.dike_reinforcements.input_profile.reinforcement_input_profile_protocol import (
@@ -24,6 +21,8 @@ from koswat.dike_reinforcements.reinforcement_layers.reinforcement_layers_wrappe
 from koswat.dike_reinforcements.reinforcement_profile.reinforcement_profile_protocol import (
     ReinforcementProfileProtocol,
 )
+
+_waterside_reinforced_points = [(-18, 0), (-18, 0), (-18, 0)]
 
 
 class TestProfileZoneCalculator:
@@ -80,17 +79,74 @@ class TestProfileZoneCalculator:
         yield reinforcement_profile_builder
 
     @pytest.mark.parametrize(
-        "points_profile, points_reinforcement, expected_zones",
+        "points_reinforcement, expected_zones",
         [
-            pytest.param([], [], (math.nan, math.nan), id="Only Zone B calculated"),
+            # Without dh0 increase, we expect zone a and zone b calculated.
             pytest.param(
-                [], [], (math.nan, math.nan), id="Zone A and Zone B calculated"
+                _waterside_reinforced_points
+                + [(0, 6), (10, 6), (39.7, -0.6), (67.7, -0.6), (74, -2)],
+                (10, 64),
+                id="Without dh0 increase - Soil reinforcement",
+            ),
+            pytest.param(
+                _waterside_reinforced_points
+                + [(0, 6), (10, 6), (43.74, -1.5), (53.75, -1.5), (56, -2)],
+                (10, 46),
+                id="Without dh0 increase - Vertical Piping Solution",
+            ),
+            pytest.param(
+                _waterside_reinforced_points
+                + [(0, 6), (10, 6), (46, -2), (46, -2), (46, -2)],
+                (10, 36),
+                id="Without dh0 increase - Piping Wall reinforcement",
+            ),
+            pytest.param(
+                _waterside_reinforced_points
+                + [(0, 6), (10, 6), (34, -2), (34, -2), (34, -2)],
+                (10, 24),
+                id="Without dh0 increase - Stability Wall reinforcement",
+            ),
+            pytest.param(
+                _waterside_reinforced_points
+                + [(0, 6), (10, 6), (34, -2), (34, -2), (34, -2)],
+                (10, 24),
+                id="Without dh0 increase - Cofferdam reinforcement",
+            ),
+            # With dh0 increase (of 1), we expect only zone b calculated.
+            pytest.param(
+                _waterside_reinforced_points
+                + [(3, 7), (13, 7), (40.87, -0.6), (68.87, -0.6), (74, -2)],
+                (math.nan, 71),
+                id="With dh0 increase to 1 - Soil reinforcement",
+            ),
+            pytest.param(
+                _waterside_reinforced_points
+                + [(3, 7), (13, 7), (44.17, -1.5), (54.17, -1.5), (56, -2)],
+                (math.nan, 53),
+                id="With dh0 increase to 1 - Vertical Piping Solution",
+            ),
+            pytest.param(
+                _waterside_reinforced_points
+                + [(3, 7), (13, 7), (46, -2), (46, -2), (46, -2)],
+                (math.nan, 43),
+                id="With dh0 increase to 1 - Piping Wall reinforcement",
+            ),
+            pytest.param(
+                _waterside_reinforced_points
+                + [(3, 7), (13, 7), (34, -2), (34, -2), (34, -2)],
+                (math.nan, 31),
+                id="With dh0 increase to 1 - Stability Wall reinforcement",
+            ),
+            pytest.param(
+                _waterside_reinforced_points
+                + [(0, 7), (10, 7), (34, -2), (34, -2), (34, -2)],
+                (math.nan, 34),
+                id="With dh0 increase to 1- Cofferdam reinforcement",
             ),
         ],
     )
     def test_given_reinforcement_profile_when_calculate_then_expected_zones(
         self,
-        points_profile: list[tuple[float]],
         points_reinforcement: list[tuple[float]],
         expected_zones: tuple[float, float],
         reinforcement_profile_builder: Callable[
@@ -98,8 +154,18 @@ class TestProfileZoneCalculator:
         ],
     ):
         # 1. Define test data.
+        _points_base_profile = [
+            (-18, 0),
+            (-18, 0),
+            (-18, 0),
+            (0, 6),
+            (10, 6),
+            (34, -2),
+            (34, -2),
+            (34, -2),
+        ]
         _reinforcement_profile = reinforcement_profile_builder(
-            points_profile, points_reinforcement
+            _points_base_profile, points_reinforcement
         )
         assert isinstance(_reinforcement_profile, ReinforcementProfileProtocol)
 
