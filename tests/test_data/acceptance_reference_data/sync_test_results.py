941b1bd1ae3c6b3f2586aac62bfeffa77ed7606b
Ardt Klapwijk <ardt.klapwijk@deltares.nl>
2024-09-20 11:27:42 +0200

    chore: extend sync script

--- tests/test_data/acceptance_reference_data/sync_test_results.py
+++ tests/test_data/acceptance_reference_data/sync_test_results.py
@@ -6,25 +6,20 @@ _test_data_path = Path(getcwd()).joinpath(r"tests\test_data\acceptance_reference
 _test_results_path = Path(getcwd()).joinpath(
     r"tests\test_results\sandbox_acceptance_case"
 )
-_costs_filename = "summary_costs.csv"
-_locations_filename = "summary_locations.csv"
 
-_test_data_folders = [f for f in _test_data_path.iterdir() if f.is_dir()]
+_test_result_folders = [f for f in _test_results_path.iterdir() if f.is_dir()]
 
-for _case in _test_data_folders:
-    _to_dir = Path(_test_data_path.joinpath(_case))
+for _from_dir in _test_result_folders:
+    _to_dir = Path(_test_data_path.joinpath(_from_dir.name))
 
-    _from_file = Path(_test_results_path.joinpath(_case.name).joinpath(_costs_filename))
-    if _from_file.exists() and _to_dir.exists():
-        copy2(_from_file, _to_dir)
-
-    _from_file = _test_results_path.joinpath(_case.name).joinpath(_locations_filename)
-    if _from_file.exists() and _to_dir.exists():
-        copy2(_from_file, _to_dir)
+    # Copy CSVs
+    for _result_file in list(_from_dir.glob("*.csv")):
+        if _result_file.is_file() and _to_dir.exists():
+            copy2(_result_file, _to_dir)
 
     # Copy figures
-    _from_dir = _test_results_path.joinpath(_case.name).joinpath("figures")
+    _from_fig_dir = _from_dir.joinpath("figures")
     _to_fig_dir = _to_dir.joinpath("figures")
-    if _from_dir.exists() and _to_fig_dir.exists():
+    if _from_fig_dir.exists() and _to_fig_dir.exists():
         rmtree(_to_fig_dir)
-        copytree(_from_dir, _to_fig_dir)
+        copytree(_from_fig_dir, _to_fig_dir)
