c7f49bf7367a3ebeba9ccafddc17fbf61c470bbc
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2024-10-23 15:07:09 +0200

    feat: export locations report into a shp file instead (#185)
    * ci: Added new dependency
    
    * chore: Added basic logic of the `shp` exporter
    
    * chore: Extended exporter to include old and new buffers
    
    * chore: Fixed small correction on exporter
    
    * docs: Added badges for releases
    
    * docs: Corrected installation typo
    
    * chore: Adapted shp exporter for dealing with `fom` dataclasses
    
    * chore: Small corrections to the code so that it will generate the correct files
    
    * chore: Corrected wrong buffering and name of parts
    
    * test: Extended tests to properly test the `ClusterCollectionShpFom.generate_geodataframes` call
    
    * chore: Extracted cluster fom dataclasses into their own files; adapted imports
    
    * test: Temporary disable acceptance tests
    
    * test: Temporary disable acceptance tests
    
    * test: Modified fixture being used for koswat summary exporter tests
    
    * test: Moved skip marker
    
    * test: Removed skip case for default sandbox cases
    
    * test: Removed skip case for default sandbox cases
    
    * test: Removed skip case for default sandbox cases
    
    * fix: in case no clusters are generated then avoid exporting to file
    
    * test: Enabled missing acceptance test
    
    * chore: Processed review remarks

---
+++ koswat/cost_report/io/summary/summary_locations/summary_locations_shp_exporter.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from koswat.core.io.koswat_exporter_protocol import KoswatExporterProtocol
+from koswat.cost_report.io.summary.summary_locations.cluster_collection_shp_fom import (
+    ClusterCollectionShpFom,
+)
+from koswat.cost_report.summary.koswat_summary import KoswatSummary
+
+
+class SummaryLocationsShpExporter(KoswatExporterProtocol):
+    def export(self, koswat_summary: KoswatSummary, export_path: Path) -> None:
+        if not export_path.exists():
+            export_path.mkdir(parents=True)
+        _measures = export_path.joinpath("summary_locations_measures.shp")
+        _old = export_path.joinpath("summary_locations_old.shp")
+        _new = export_path.joinpath("summary_locations_new.shp")
+
+        # Get clusters
+        _clusters = ClusterCollectionShpFom.from_summary(
+            koswat_summary
+        ).generate_geodataframes()
+
+        if not _clusters:
+            return
+
+        _clusters[0].to_file(_measures)
+        _clusters[1].to_file(_old)
+        _clusters[2].to_file(_new)
---
+++ tests/cost_report/io/summary/summary_costs/test_summary_locations_shp_exporter.py
@@ -0,0 +1,35 @@
+import shutil
+
+import pytest
+
+from koswat.core.io.koswat_exporter_protocol import KoswatExporterProtocol
+from koswat.cost_report.io.summary.summary_locations.summary_locations_shp_exporter import (
+    SummaryLocationsShpExporter,
+)
+from koswat.cost_report.summary.koswat_summary import KoswatSummary
+from tests import test_results
+
+
+class TestSummaryLocationsShpExporter:
+    def test_initialize(self):
+        _exporter = SummaryLocationsShpExporter()
+        assert isinstance(_exporter, SummaryLocationsShpExporter)
+        assert isinstance(_exporter, KoswatExporterProtocol)
+
+    def test_summary_locations_shp_exporter_export(
+        self,
+        valid_clusters_mocked_summary: KoswatSummary,
+        request: pytest.FixtureRequest,
+    ):
+        # 1. Define test data.
+        _test_dir = test_results.joinpath(request.node.name)
+        if _test_dir.is_dir():
+            shutil.rmtree(_test_dir)
+
+        # 2. Run test.
+        SummaryLocationsShpExporter().export(valid_clusters_mocked_summary, _test_dir)
+
+        # 3. Verify expectations.
+        assert _test_dir.exists()
+        _shp_files = list(_test_dir.glob("*.shp"))
+        assert len(_shp_files) == 3
