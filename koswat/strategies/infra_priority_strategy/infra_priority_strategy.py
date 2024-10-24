51a086c56fd5acb84b0fa3aa938933a71a3c0030
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2024-10-24 23:14:43 +0200

    chore: minor algorithm improvement

--- koswat/strategies/infra_priority_strategy/infra_priority_strategy.py
+++ koswat/strategies/infra_priority_strategy/infra_priority_strategy.py
@@ -13,7 +13,7 @@ class InfraPriorityStrategy(StrategyProtocol):
     def apply_strategy(
         self, strategy_input: StrategyInput
     ) -> list[StrategyLocationReinforcement]:
-        # First apply `OrderStrategy` to generate an initial cluster formation.
+        # 1. Run `OrderStrategy` to generate an initial cluster formation.
         _initial_clusters = OrderStrategy().apply_strategy(strategy_input)
 
         # This strategy works under the assumption that \
@@ -23,6 +23,7 @@ class InfraPriorityStrategy(StrategyProtocol):
         # per cluster.
 
         _result_clusters = []
+
         for _initial_measure, _cluster in groupby(
             _initial_clusters, key=lambda x: x.selected_measure
         ):
@@ -30,28 +31,38 @@ class InfraPriorityStrategy(StrategyProtocol):
             if not any(_cluster_data):
                 continue
 
-            _common_available_cluster_measures = set(
-                _cluster_data[0].available_measures
-            )
+            # 2. Get common available measures for each cluster
+            _common_available_measures = set(_cluster_data[0].available_measures)
+            _common_available_measures.discard(_common_available_measures)
+            # _initial_costs = sum(_c.current_cost for _c in _cluster_data)
 
+            _selection = _initial_measure
             _costs_dict = defaultdict(lambda: 0.0)
             _costs_dict[_initial_measure] = sum(_c.current_cost for _c in _cluster_data)
-            for _cd in _cluster_data:
-                _common_available_cluster_measures.intersection_update(
-                    _cd.available_measures
-                )
-                for _am in _common_available_cluster_measures:
-                    if _am == _initial_measure:
-                        continue
-                    _costs_dict[_am] += _cluster_data[0].get_reinforcement_costs(_am)
-            _selection = _initial_measure
-            for _ca in _common_available_cluster_measures:
-                if (
-                    _ca != _initial_measure
-                    and _costs_dict[_ca] < _costs_dict[_selection]
-                ):
-                    _selection = _ca
 
+            # Define common available measures
+            for _cd in _cluster_data:
+                _common_available_measures.intersection_update(_cd.available_measures)
+                _discarded_measures = set()
+                # Get only available measures **cheaper** than current state
+                # and their cost summation.
+                for _am in _common_available_measures:
+                    _costs_dict[_am] += _cluster_data[0].get_reinforcement_costs(_am)
+                    if _costs_dict[_am] > _costs_dict[_initial_measure]:
+                        # This can potentially save (a lot) of computations.
+                        _discarded_measures.add(_am)
+                        continue
+                _common_available_measures -= _discarded_measures
+
+            # 3. Set the **cheapest** common available measure.
+            # 3.1. Get the cheapest common available measure.
+            _common_available_measures.add(_initial_measure)
+            _selection = min(
+                _common_available_measures,
+                key=lambda x: _costs_dict[x],
+            )
+
+            # 3.2. Set the selection if different from the initial.
             if _selection != _initial_measure:
                 for _cd in _cluster_data:
                     _cd.selected_measure = _selection
