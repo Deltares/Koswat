7f9bb508f410ffac50679e6b441916c9dfdaf454
Ardt Klapwijk <ardt.klapwijk@deltares.nl>
2024-10-28 12:15:39 +0100

    test: correct export output

--- tests/cost_report/io/summary/summary_costs/test_summary_costs_csv_exporter.py
+++ tests/cost_report/io/summary/summary_costs/test_summary_costs_csv_exporter.py
@@ -73,8 +73,8 @@ Construction length (cost incl surtax):;0.0;0.0;0.0;0.0
 Total measure meters;0;1;1;2
 Total measure cost;0.0;8.14;16.29;48.87;73.3
 Total measure cost incl surtax;0.0;12.22;24.43;73.3;109.95
-Infrastructure cost;0.0;0.0;6.6;33.0;39.6
-Infrastructure cost incl surtax;0.0;0.0;19.8;257.4;277.2
-Total cost;0.0;8.14;22.89;81.87;112.9
-Total cost incl surtax;0.0;12.22;44.23;330.7;387.15"""
+Infrastructure cost;0.0;0.0;6.6;39.6;46.2
+Infrastructure cost incl surtax;0.0;0.0;19.8;277.2;297.0
+Total cost;0.0;8.14;22.89;88.47;119.5
+Total cost incl surtax;0.0;12.22;44.23;350.5;406.95"""
         assert _expected_text == _read_text
