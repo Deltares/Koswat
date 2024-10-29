8d6c48df030d2cbac42593dcc0d0d8f5acfea3b2
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2024-10-29 11:51:57 +0100

    test: Extended tests for cluster option

--- tests/strategies/infra_priority_stratregy/test_infra_cluster_option.py
+++ tests/strategies/infra_priority_stratregy/test_infra_cluster_option.py
@@ -2,9 +2,18 @@ from typing import Iterator
 
 import pytest
 
+from koswat.dike_reinforcements.reinforcement_profile.standard.piping_wall_reinforcement_profile import (
+    PipingWallReinforcementProfile,
+)
+from koswat.dike_reinforcements.reinforcement_profile.standard.soil_reinforcement_profile import (
+    SoilReinforcementProfile,
+)
 from koswat.strategies.infra_priority_strategy.infra_cluster import InfraCluster
 from koswat.strategies.infra_priority_strategy.infra_cluster_option import (
     InfraClusterOption,
+)
+from koswat.strategies.strategy_location_reinforcement import (
+    StrategyLocationReinforcement,
 )
 
 
@@ -64,3 +73,43 @@ class TestInfraClusterOption:
 
         # 3. Verify expectations.
         assert dummy_option.valid_option()
+
+    @pytest.fixture(name="valid_option")
+    def _get_valid_option_fixture(self) -> Iterator[InfraClusterOption]:
+        _option = InfraClusterOption(cluster_min_length=1)
+        _option.add_cluster(
+            InfraCluster(
+                reinforcement_type=SoilReinforcementProfile,
+                min_required_length=0,
+                cluster=[
+                    StrategyLocationReinforcement(
+                        location=None,
+                        selected_measure=SoilReinforcementProfile,
+                        available_measures=[],
+                    )
+                ],
+            ),
+            {SoilReinforcementProfile: 42000, PipingWallReinforcementProfile: 42},
+        )
+        yield _option
+
+    def test_given_valid_cluster_when_set_cheapest_option_then_updates_clusters(
+        self, valid_option: InfraClusterOption
+    ):
+        # 1. Define test data.
+        _current_selection = SoilReinforcementProfile
+        _new_selection = PipingWallReinforcementProfile
+        assert valid_option.valid_option()
+        assert all(
+            _oc.reinforcement_type == _current_selection
+            for _oc in valid_option.cluster_collection
+        )
+
+        # 2. Run test.
+        valid_option.set_cheapest_option()
+
+        # 3. Verify expectations.
+        assert all(
+            _oc.reinforcement_type == _new_selection
+            for _oc in valid_option.cluster_collection
+        )
