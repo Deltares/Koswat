327f5c94e667145565597f46311012f74f575c7a
Ardt Klapwijk <ardt.klapwijk@deltares.nl>
2024-11-04 12:13:19 +0100

    chore: fix linting issue

--- tests/cost_report/io/summary/summary_costs/test_summary_costs_csv_exporter.py
+++ tests/cost_report/io/summary/summary_costs/test_summary_costs_csv_exporter.py
@@ -30,7 +30,7 @@ class TestSummaryCostsCsvExporter:
 
         # 3. Validate results
         assert _export_path.exists()
-        _read_text = _export_path.read_text()
+        _read_text = _export_path.read_text(encoding="utf-8")
         _expected_text = """Profile type;Kistdam;Kwelscherm;Grondmaatregel profiel;Stabiliteitswand
 Strategy reinforcement order;-1;-1;-1;-1
 Cost per km (Euro/km);0.0;8144.4;16288.8;24433.2
