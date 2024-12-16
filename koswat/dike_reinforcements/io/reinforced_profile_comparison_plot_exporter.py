eaa2a5458de0dcc5ca4e63c5e04d6566a1fa2636
Ardt Klapwijk <ardt.klapwijk@deltares.nl>
2024-12-16 11:53:03 +0100

    chore: add dijkvak to export name

--- koswat/dike_reinforcements/io/reinforced_profile_comparison_plot_exporter.py
+++ koswat/dike_reinforcements/io/reinforced_profile_comparison_plot_exporter.py
@@ -13,9 +13,10 @@ class ReinforcedProfileComparisonPlotExporter(PlotExporterProtocol):
     reinforced_profile: ReinforcementProfileProtocol
 
     def export(self) -> None:
+        _section_name = self.reinforced_profile.old_profile.input_data.dike_section
         self.export_dir.mkdir(parents=True, exist_ok=True)
         _file_path = self.export_dir.joinpath(
-            str(self.reinforced_profile).replace(" ", "_")
+            f"{_section_name}_{self.reinforced_profile}"
         ).with_suffix(".png")
 
         # Define canvas
