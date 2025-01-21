a60fee7dd182700757ba598ba5863a8d7a2e368c
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2025-01-21 10:21:42 +0000

    ci: Updated scripts, I'm done with this :D

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash poetry install
-# This does not seem to be working due to the line ending (LF instead of CRLF)
-# poetry install
-# git config --global --add safe.directory "/usr/src/app"
-# git config --global core.autocrlf true 
+#!/usr/bin/env bash
+poetry install 
+git config --global --add safe.directory "/usr/src/app"
+git config --global core.autocrlf true
