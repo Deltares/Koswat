d45b485b215c8d0147810051289375eef29c65de
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2025-01-21 10:13:02 +0000

    ci: postcommand script as one line

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
-poetry install
-git config --global --add safe.directory "/usr/src/app"
-git config --global core.autocrlf true 
+#!/usr/bin/env bash poetry install
+# This does not seem to be working due to the line ending (LF instead of CRLF)
+# poetry install
+# git config --global --add safe.directory "/usr/src/app"
+# git config --global core.autocrlf true 
