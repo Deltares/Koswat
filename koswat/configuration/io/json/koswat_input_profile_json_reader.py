ec6e4bcad964a6bf61baef902190220657ba314a
ArdtK <ardt.klapwijk@deltares.nl>
2025-10-21 16:40:01 +0200

    chore: update change from master

--- koswat/configuration/io/json/koswat_input_profile_json_reader.py
+++ koswat/configuration/io/json/koswat_input_profile_json_reader.py
@@ -9,7 +9,16 @@ from koswat.core.io.koswat_reader_protocol import KoswatReaderProtocol
 
 
 class KoswatInputProfileJsonReader(KoswatReaderProtocol):
+    def supports_file(self, file_path: Path) -> bool:
+        return isinstance(file_path, Path) and file_path.suffix == ".json"
+
     def read(self, file_path: Path) -> KoswatInputProfileJsonFom:
+        if not self.supports_file(file_path):
+            raise ValueError("Json file should be provided")
+
+        if not file_path.is_file():
+            raise FileNotFoundError(file_path)
+
         _json_fom = KoswatJsonReader().read(file_path)
 
         def _format_headers(input_dict: dict[str, Any]) -> dict[str, Any]:
