6c749b69551e153664287b0dcbfa20845d8c067a
ArdtK <ardt.klapwijk@deltares.nl>
2025-10-21 15:43:41 +0200

    chore: implement logic

--- koswat/configuration/io/koswat_input_profile_list_importer.py
+++ koswat/configuration/io/koswat_input_profile_list_importer.py
@@ -1,3 +1,5 @@
+import logging
+from dataclasses import dataclass, field
 from pathlib import Path
 
 from koswat.configuration.io.json.koswat_input_profile_json_reader import (
@@ -7,7 +9,10 @@ from koswat.core.io.koswat_importer_protocol import KoswatImporterProtocol
 from koswat.dike.profile.koswat_input_profile_base import KoswatInputProfileBase
 
 
+@dataclass(kw_only=True)
 class KoswatInputProfileListImporter(KoswatImporterProtocol):
+    dike_selection: list[str] = field(default_factory=list)
+
     def _get_koswat_input_profile_base(
         self, fom_dict: dict[str, str | float]
     ) -> KoswatInputProfileBase:
@@ -34,6 +39,11 @@ class KoswatInputProfileListImporter(KoswatImporterProtocol):
     def import_from(self, from_path: Path) -> list[KoswatInputProfileBase]:
         _profile_input_list = []
         for file in from_path.glob("*.json"):
+            if self.dike_selection and file.stem not in self.dike_selection:
+                logging.error(
+                    "Dike profile %s skipped because section was not selected.",
+                    file.stem,
+                )
             _profile_input = KoswatInputProfileJsonReader().read(file)
             _profile_input_list.append(
                 self._get_koswat_input_profile_base(_profile_input.input_profile_fom)
