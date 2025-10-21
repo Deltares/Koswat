eac982c6457665a7133b592afce6894bb131a30e
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2025-10-21 15:58:11 +0200

    chore: #264 replace current csv input by json input (#271)
    * chore: move/rename io modules
    
    * chore: rename/move tests
    
    * chore: rename test data file
    
    * chore: add test data
    
    * chore: rename modules to singular
    
    * chore: move files
    
    * chore: refactor reader
    
    * chore: add/fix tests
    
    * chore: move/rename io modules
    
    * chore: rename/move tests
    
    * chore: rename test data file
    
    * chore: add test data
    
    * chore: rename modules to singular
    
    * chore: move files
    
    * chore: refactor reader
    
    * chore: add/fix tests
    
    * chore: fix test issues
    
    * chore: remove outdated module
    
    * chore: fix failures
    
    * chore: fix issue
    
    * chore: small improvements
    
    * chore: implement supports_file
    
    * chore: process review remarks

---
+++ koswat/core/io/json/koswat_json_reader.py
@@ -0,0 +1,22 @@
+import json
+from pathlib import Path
+
+from koswat.core.io.json.koswat_json_fom import KoswatJsonFom
+from koswat.core.io.koswat_reader_protocol import KoswatReaderProtocol
+
+
+class KoswatJsonReader(KoswatReaderProtocol):
+    def supports_file(self, file_path: Path) -> bool:
+        return isinstance(file_path, Path) and file_path.suffix == ".json"
+
+    def read(self, file_path: Path) -> KoswatJsonFom:
+        if not self.supports_file(file_path):
+            raise ValueError("Json file should be provided")
+
+        if not file_path.is_file():
+            raise FileNotFoundError(file_path)
+
+        with open(file_path, "r", encoding="utf-8") as _file:
+            _json_content = json.load(_file)
+
+        return KoswatJsonFom(file_stem=file_path.stem, content=_json_content)
