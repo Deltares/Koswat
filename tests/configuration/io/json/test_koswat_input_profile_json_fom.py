11f788ede4934b1e3122f62118aaf2f111cf275d
ArdtK <ardt.klapwijk@deltares.nl>
2025-10-23 17:42:09 +0200

    chore: fix tests

--- tests/configuration/io/json/test_koswat_input_profile_json_fom.py
+++ tests/configuration/io/json/test_koswat_input_profile_json_fom.py
@@ -6,8 +6,10 @@ from koswat.core.io.file_object_model_protocol import FileObjectModelProtocol
 
 class TestKoswatInputProfileJsonFom:
     def test_initialize(self):
+        # 1. Execute test
         _csv_fom = KoswatDikeSectionInputJsonFom()
+
+        # 2. Verify expectations
         assert isinstance(_csv_fom, KoswatDikeSectionInputJsonFom)
         assert isinstance(_csv_fom, FileObjectModelProtocol)
-        assert _csv_fom.input_profile == {}
         assert not _csv_fom.is_valid()
