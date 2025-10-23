533a35cfdb71a4232552360a69d94f03632b4caa
ArdtK <ardt.klapwijk@deltares.nl>
2025-10-23 14:41:33 +0200

    chore: fix foms

--- koswat/configuration/io/config_sections/dike_profile_section_fom.py
+++ koswat/configuration/io/config_sections/dike_profile_section_fom.py
@@ -4,11 +4,12 @@ from koswat.configuration.io.config_sections.config_section_fom_base import (
 
 
 class DikeProfileSectionFom(ConfigSectionFomBase):
+    dike_section: str = ""
     thickness_grass_layer: float
     thickness_clay_layer: float
 
-    _float_mappings: dict = dict(
+    _float_mappings = dict(
         dikte_graslaag="thickness_grass_layer",
         dikte_kleilaag="thickness_clay_layer",
     )
-    _surtax_mappings: dict = dict()
+    _surtax_mappings = dict()
