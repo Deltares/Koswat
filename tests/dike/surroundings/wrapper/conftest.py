d2c458cebc2c469a2e8c90c43444771c0bfd324e
Carles S. Soriano Pérez <sorianoperez.carles@gmail.com>
2025-10-28 14:38:48 +0100

    test: Fixed failing tests

--- tests/dike/surroundings/wrapper/conftest.py
+++ tests/dike/surroundings/wrapper/conftest.py
@@ -3,7 +3,7 @@ from typing import Callable, Iterable
 import pytest
 from shapely import Point
 
-from koswat.dike.surroundings.point.point_surroundings import PointSurroundings
+from koswat.dike.surroundings.point.point_obstacle_surroundings import PointObstacleSurroundings
 from koswat.dike.surroundings.surroundings_obstacle import SurroundingsObstacle
 from koswat.dike.surroundings.wrapper.obstacle_surroundings_wrapper import (
     ObstacleSurroundingsWrapper,
@@ -12,14 +12,15 @@ from koswat.dike.surroundings.wrapper.obstacle_surroundings_wrapper import (
 
 @pytest.fixture(name="buildings_obstacle_surroundings_fixture")
 def _get_buildings_fixture() -> Iterable[
-    Callable[[list[tuple[Point, list[float]]]], ObstacleSurroundingsWrapper]
+    Callable[[list[tuple[Point, float]]], ObstacleSurroundingsWrapper]
 ]:
     def build_surroundings_wrapper(
-        point_distances: list[tuple[Point, list[float]]]
+        point_distances: list[tuple[Point, float]]
     ) -> ObstacleSurroundingsWrapper:
         _points = [
-            PointSurroundings(
-                location=_pd[0], surroundings_matrix={_d: 1 for _d in _pd[1]}
+            PointObstacleSurroundings(
+                location=_pd[0],
+                inside_distance=_pd[1],
             )
             for _pd in point_distances
         ]
