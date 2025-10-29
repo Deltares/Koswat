f7aa220b04d68e37be522ba366ff30d741466876
ArdtK <ardt.klapwijk@deltares.nl>
2025-10-29 11:00:00 +0100

    chore: fix protocol

--- koswat/configuration/io/config_sections/config_section_fom_protocol.py
+++ koswat/configuration/io/config_sections/config_section_fom_protocol.py
@@ -1,16 +1,15 @@
-import abc
-from typing import Any
+from typing import Any, Protocol, runtime_checkable
 
 from koswat.core.io.file_object_model_protocol import FileObjectModelProtocol
 
 
-class ConfigSectionFomProtocol(FileObjectModelProtocol, abc.ABC):
+@runtime_checkable
+class ConfigSectionFomProtocol(FileObjectModelProtocol, Protocol):
     """
     Protocol for configuration section file object models.
     """
 
     @classmethod
-    @abc.abstractmethod
     def from_config(cls, input_dict: dict[str, Any]) -> "ConfigSectionFomProtocol":
         """
         Create an instance of the class from a dictionary, not setting defaults.
@@ -23,7 +22,6 @@ class ConfigSectionFomProtocol(FileObjectModelProtocol, abc.ABC):
         """
 
     @classmethod
-    @abc.abstractmethod
     def from_config_set_defaults(
         cls, input_dict: dict[str, Any]
     ) -> "ConfigSectionFomProtocol":
@@ -37,7 +35,6 @@ class ConfigSectionFomProtocol(FileObjectModelProtocol, abc.ABC):
             FileObjectModelProtocol: The created instance.
         """
 
-    @abc.abstractmethod
     def merge(self, other: "ConfigSectionFomProtocol") -> "ConfigSectionFomProtocol":
         """
         Merge another ConfigSectionFomProtocol into this one.
