fb12fd854de1b168afff957168963dffcb0a4340
ArdtK <ardt.klapwijk@deltares.nl>
2025-10-30 11:34:04 +0100

    chore: fix conftest

--- tests/configuration/io/conftest.py
+++ tests/configuration/io/conftest.py
@@ -1,3 +1,4 @@
+from os import unlink
 from pathlib import Path
 from shutil import rmtree
 from typing import Iterator
@@ -13,6 +14,7 @@ def empty_dir() -> Iterator[Path]:
     if _temp_dir.exists():
         rmtree(_temp_dir)
     _temp_dir.mkdir(parents=True, exist_ok=True)
+    assert _temp_dir.is_dir()
 
     yield _temp_dir
 
@@ -24,10 +26,11 @@ def empty_file() -> Iterator[Path]:
     _temp_dir = test_results
     _temp_dir.mkdir(parents=True, exist_ok=True)
     _temp_file = _temp_dir.joinpath("empty_file.txt")
-    if _temp_file.exists():
-        _temp_file.unlink()
+    _temp_file.unlink(missing_ok=True)
     _temp_file.touch()
+
+    assert _temp_file.is_file()
 
     yield _temp_file
 
-    rmtree(_temp_file)
+    _temp_file.unlink(missing_ok=True)
