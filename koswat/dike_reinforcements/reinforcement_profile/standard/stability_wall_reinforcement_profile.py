4ae32c31c4e4a27f1dff396b4da63d2c3d1c17c7
Carles S. Soriano Pérez <sorianoperez.carles@gmail.com>
2025-12-01 10:41:17 +0100

    docs: Updated license headers for source files

--- koswat/dike_reinforcements/reinforcement_profile/standard/stability_wall_reinforcement_profile.py
+++ koswat/dike_reinforcements/reinforcement_profile/standard/stability_wall_reinforcement_profile.py
@@ -1,3 +1,24 @@
+"""
+                    GNU GENERAL PUBLIC LICENSE
+                      Version 3, 29 June 2007
+
+    KOSWAT, from the dutch combination of words `Kosts-Wat` (what are the costs)
+    Copyright (C) 2024 Stichting Deltares
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from koswat.dike.koswat_profile_protocol import KoswatProfileProtocol
 from koswat.dike_reinforcements.input_profile.stability_wall.stability_wall_input_profile import (
     StabilityWallInputProfile,
