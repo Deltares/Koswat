4b5988fd8f4d2a2d0e663e3b7ca869970599da5c
ArdtK <ardt.klapwijk@deltares.nl>
2026-03-24 10:48:42 +0100

    chore: pass properties to export modules

--- koswat/cost_report/io/summary/koswat_summary_exporter.py
+++ koswat/cost_report/io/summary/koswat_summary_exporter.py
@@ -1,22 +1,22 @@
 """
-                    GNU GENERAL PUBLIC LICENSE
-                      Version 3, 29 June 2007
+                GNU GENERAL PUBLIC LICENSE
+                  Version 3, 29 June 2007
 
-    KOSWAT, from the dutch combination of words `Kosts-Wat` (what are the costs)
-    Copyright (C) 2025 Stichting Deltares
+KOSWAT, from the dutch combination of words `Kosts-Wat` (what are the costs)
+Copyright (C) 2025 Stichting Deltares
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import logging
@@ -39,22 +39,31 @@ from koswat.cost_report.summary.koswat_summary import KoswatSummary
 
 
 class KoswatSummaryExporter(KoswatExporterProtocol):
-    def export(self, koswat_summary: KoswatSummary, export_path: Path) -> None:
+    koswat_summary: KoswatSummary
+    export_path: Path
+    export_shapefiles: bool
+
+    def export(self) -> None:
         SummaryCostsCsvExporter().export(
-            koswat_summary, export_path.joinpath("summary_costs.csv")
+            self.koswat_summary, self.export_path.joinpath("summary_costs.csv")
         )
         SummaryInfrastructureCostsCsvExporter().export(
-            koswat_summary, export_path.joinpath("summary_infrastructure_costs.csv")
+            self.koswat_summary,
+            self.export_path.joinpath("summary_infrastructure_costs.csv"),
         )
         SummaryLocationsCsvExporter().export(
-            koswat_summary, export_path.joinpath("summary_locations.csv")
+            self.koswat_summary, self.export_path.joinpath("summary_locations.csv")
         )
+
+        if not self.export_shapefiles:
+            return
+
         try:
             SummaryLocationsShpExporter().export(
-                koswat_summary, export_path.joinpath("summary_locations")
+                self.koswat_summary, self.export_path.joinpath("summary_locations")
             )
         except:
             logging.error(
                 "Shapefile error!: %s",
-                export_path._str,
+                self.export_path,
             )
