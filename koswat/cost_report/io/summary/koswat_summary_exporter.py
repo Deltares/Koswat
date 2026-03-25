7177a143149e6924dfe6b0546afc1444f8d17eb0
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2026-03-25 08:11:30 +0100

    feat: #334 add settings for export of pngs and shapefiles (#354)
    * chore: add settings to config
    
    * chore: pass properties to export modules
    
    * chore: rewrite to dataclass
    
    * chore: fix test
    
    * chore: fix test
    
    * chore: update md

--- koswat/cost_report/io/summary/koswat_summary_exporter.py
+++ koswat/cost_report/io/summary/koswat_summary_exporter.py
@@ -1,25 +1,26 @@
 """
-                    GNU GENERAL PUBLIC LICENSE
-                      Version 3, 29 June 2007
+                GNU GENERAL PUBLIC LICENSE
+                  Version 3, 29 June 2007
 
-    KOSWAT, from the dutch combination of words `Kosts-Wat` (what are the costs)
-    Copyright (C) 2025 Stichting Deltares
+KOSWAT, from the dutch combination of words `Kosts-Wat` (what are the costs)
+Copyright (C) 2025 Stichting Deltares
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import logging
+from dataclasses import dataclass, field
 from pathlib import Path
 
 from koswat.core.io.koswat_exporter_protocol import KoswatExporterProtocol
@@ -38,23 +39,33 @@ from koswat.cost_report.io.summary.summary_locations.summary_locations_shp_expor
 from koswat.cost_report.summary.koswat_summary import KoswatSummary
 
 
+@dataclass(kw_only=True)
 class KoswatSummaryExporter(KoswatExporterProtocol):
-    def export(self, koswat_summary: KoswatSummary, export_path: Path) -> None:
+    koswat_summary: KoswatSummary = field(default_factory=KoswatSummary)
+    export_path: Path = field(default_factory=Path)
+    export_shapefiles: bool = True
+
+    def export(self) -> None:
         SummaryCostsCsvExporter().export(
-            koswat_summary, export_path.joinpath("summary_costs.csv")
+            self.koswat_summary, self.export_path.joinpath("summary_costs.csv")
         )
         SummaryInfrastructureCostsCsvExporter().export(
-            koswat_summary, export_path.joinpath("summary_infrastructure_costs.csv")
+            self.koswat_summary,
+            self.export_path.joinpath("summary_infrastructure_costs.csv"),
         )
         SummaryLocationsCsvExporter().export(
-            koswat_summary, export_path.joinpath("summary_locations.csv")
+            self.koswat_summary, self.export_path.joinpath("summary_locations.csv")
         )
+
+        if not self.export_shapefiles:
+            return
+
         try:
             SummaryLocationsShpExporter().export(
-                koswat_summary, export_path.joinpath("summary_locations")
+                self.koswat_summary, self.export_path.joinpath("summary_locations")
             )
         except:
             logging.error(
                 "Shapefile error!: %s",
-                export_path._str,
+                self.export_path,
             )
