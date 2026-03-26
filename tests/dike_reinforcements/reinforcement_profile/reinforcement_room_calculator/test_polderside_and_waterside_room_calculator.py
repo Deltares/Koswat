20580826fca4a42469b86d25fbd75b5783d4a08c
ArdtK <ardt.klapwijk@deltares.nl>
2026-03-26 12:46:34 +0100

    chore: adjust tests

--- tests/dike_reinforcements/reinforcement_profile/reinforcement_room_calculator/test_polderside_and_waterside_room_calculator.py
+++ tests/dike_reinforcements/reinforcement_profile/reinforcement_room_calculator/test_polderside_and_waterside_room_calculator.py
@@ -1,21 +1,32 @@
 import pytest
-from koswat.dike.surroundings.point.point_obstacle_surroundings import PointObstacleSurroundings
-from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator.polderside_and_waterside_room_calculator import PoldersideAndWatersideRoomCalculator
-from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator.reinforcement_room_calculator_base import ReinforcementRoomCalculatorBase
-from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator.reinforcement_room_calculator_protocol import ReinforcementRoomCalculatorProtocol
+
+from koswat.dike.surroundings.point.point_obstacle_surroundings import (
+    PointObstacleSurroundings,
+)
+from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator.polderside_and_waterside_room_calculator import (
+    PoldersideAndWatersideRoomCalculator,
+)
+from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator.reinforcement_room_calculator_base import (
+    ReinforcementRoomCalculatorBase,
+)
+from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator.reinforcement_room_calculator_protocol import (
+    ReinforcementRoomCalculatorProtocol,
+)
+
 
 def get_reinforcement_and_obstacle_room_cases() -> list[pytest.param]:
     _polderside_width = 4
     _waterside_width = 2
     _base_calculator = PoldersideAndWatersideRoomCalculator(
-                required_polderside_width=_polderside_width,
-                required_waterside_width=_waterside_width
-            )
-    
-    def get_point_obstacle_surroundings(inside_space: float, outside_space: float) -> PointObstacleSurroundings:
+        required_polderside_width=_polderside_width,
+        required_waterside_width=_waterside_width,
+    )
+
+    def get_point_obstacle_surroundings(
+        inside_space: float, outside_space: float
+    ) -> PointObstacleSurroundings:
         return PointObstacleSurroundings(
-            inside_distance=inside_space,
-            outside_distance=outside_space
+            inside_distance=inside_space, outside_distance=outside_space
         )
 
     return [
@@ -23,55 +34,64 @@ def get_reinforcement_and_obstacle_room_cases() -> list[pytest.param]:
             _base_calculator,
             get_point_obstacle_surroundings(_polderside_width, _waterside_width),
             True,
-            id="exact_room_at_both_sides"
+            id="exact_room_at_both_sides",
         ),
         pytest.param(
             _base_calculator,
-            get_point_obstacle_surroundings(_polderside_width + 1, _waterside_width + 1),
+            get_point_obstacle_surroundings(
+                _polderside_width + 1, _waterside_width + 1
+            ),
             True,
-            id="enough_room_at_both_sides"
+            id="enough_room_at_both_sides",
         ),
         pytest.param(
             _base_calculator,
             get_point_obstacle_surroundings(_polderside_width + _waterside_width, 0),
             True,
-            id="enough_room_only_at_polderside"
+            id="enough_room_only_at_polderside",
         ),
         pytest.param(
             _base_calculator,
             get_point_obstacle_surroundings(0, _polderside_width + _waterside_width),
             True,
-            id="enough_room_only_at_waterside"
+            id="enough_room_only_at_waterside",
         ),
         pytest.param(
             _base_calculator,
-            get_point_obstacle_surroundings(_polderside_width - 2, _waterside_width + 2),
+            get_point_obstacle_surroundings(
+                _polderside_width - 2, _waterside_width + 2
+            ),
             True,
-            id="more_room_on_the_waterside"
+            id="more_room_on_the_waterside",
         ),
         pytest.param(
             _base_calculator,
-            get_point_obstacle_surroundings(_polderside_width + 1, _waterside_width - 1),
+            get_point_obstacle_surroundings(
+                _polderside_width + 1, _waterside_width - 1
+            ),
             True,
-            id="more_room_on_the_polderside"
+            id="more_room_on_the_polderside",
         ),
         pytest.param(
             _base_calculator,
-            get_point_obstacle_surroundings(_polderside_width - 1, _waterside_width - 1),
+            get_point_obstacle_surroundings(
+                _polderside_width - 1, _waterside_width - 1
+            ),
             False,
-            id="not_enough_room_at_either_side"
-        )
+            id="not_enough_room_at_either_side",
+        ),
     ]
 
+
 reinforcement_point_obstacle_cases = get_reinforcement_and_obstacle_room_cases()
+
 
 class TestPoldersideAndWatersideRoomCalculator:
 
     def test_when_initialize_then_returns_instance(self):
         # 1. When
         _calculator = PoldersideAndWatersideRoomCalculator(
-            required_polderside_width=1.0,
-            required_waterside_width=2.0
+            required_polderside_width=1.0, required_waterside_width=2.0
         )
 
         # 2. Then
@@ -79,13 +99,15 @@ class TestPoldersideAndWatersideRoomCalculator:
         assert isinstance(_calculator, ReinforcementRoomCalculatorBase)
         assert isinstance(_calculator, ReinforcementRoomCalculatorProtocol)
 
-    def test_when__required_width_given_calculator_then_returns_summation_of_widths(self):
+    def test_when__required_width_given_calculator_then_returns_summation_of_widths(
+        self,
+    ):
         # 1. Given
         _polderside_width = 1.0
         _waterside_width = 2.0
         _calculator = PoldersideAndWatersideRoomCalculator(
             required_polderside_width=_polderside_width,
-            required_waterside_width=_waterside_width
+            required_waterside_width=_waterside_width,
         )
 
         # 2. When
@@ -94,15 +116,63 @@ class TestPoldersideAndWatersideRoomCalculator:
         # 3. Then
         assert result == (_polderside_width + _waterside_width)
 
-
-    @pytest.mark.parametrize("calculator, surroundings, expected_result", reinforcement_point_obstacle_cases)
-    def test_when_reinforcement_has_room_given_point_obstacle_surroundings_then_returns_expectation(self, calculator: PoldersideAndWatersideRoomCalculator, surroundings: PointObstacleSurroundings, expected_result: bool):
+    @pytest.mark.parametrize(
+        "calculator, surroundings, expected_result", reinforcement_point_obstacle_cases
+    )
+    def test_when_reinforcement_has_room_given_point_obstacle_surroundings_then_returns_expectation(
+        self,
+        calculator: PoldersideAndWatersideRoomCalculator,
+        surroundings: PointObstacleSurroundings,
+        expected_result: bool,
+    ):
         # 1. Given
         assert isinstance(calculator, PoldersideAndWatersideRoomCalculator)
         assert isinstance(surroundings, PointObstacleSurroundings)
 
+        _obstacle_buffer = 0.0
+
         # 2. When
-        _result = calculator.reinforcement_has_room(surroundings)
+        _result = calculator.reinforcement_has_room(surroundings, _obstacle_buffer)
 
         # 3. Then
         assert _result == expected_result
+
+    @pytest.mark.parametrize(
+        "obstacle_distance, obstacle_buffer, is_location_safe",
+        [
+            pytest.param(
+                25, 4, True, id="Obstacle with buffer > distance, safe location"
+            ),
+            pytest.param(
+                20, 0, True, id="Obstacle without buffer > distance, safe location"
+            ),
+            pytest.param(
+                20, 2, True, id="Obstacle with buffer == distance, safe location"
+            ),
+            pytest.param(
+                20, 4, False, id="Obstacle with buffer < distance, unsafe location"
+            ),
+            pytest.param(
+                15, 0, False, id="Obstacle without buffer < distance, unsafe location"
+            ),
+        ],
+    )
+    def test_when_reinforcement_has_room_given_point_obstacle_surroundings_with_buffer_then_returns_expectation(
+        self, obstacle_distance: float, obstacle_buffer: float, is_location_safe: bool
+    ):
+        # 1. Given
+        _distance_to_check = 18
+
+        _calculator = PoldersideAndWatersideRoomCalculator(
+            required_polderside_width=_distance_to_check,
+            required_waterside_width=_distance_to_check,
+        )
+        _surroundings = PointObstacleSurroundings(
+            inside_distance=obstacle_distance, outside_distance=obstacle_distance
+        )
+
+        # 2. When
+        _result = _calculator.reinforcement_has_room(_surroundings, obstacle_buffer)
+
+        # 3. Then
+        assert _result == is_location_safe
--- tests/dike_reinforcements/reinforcement_profile/reinforcement_room_calculator/test_polderside_only_room_calculator.py
+++ tests/dike_reinforcements/reinforcement_profile/reinforcement_room_calculator/test_polderside_only_room_calculator.py
@@ -1,58 +1,76 @@
-from koswat.dike.surroundings.point.point_obstacle_surroundings import PointObstacleSurroundings
-from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator.polderside_only_room_calculator import PoldersideOnlyRoomCalculator
+import math
+
 import pytest
 
-from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator.reinforcement_room_calculator_base import ReinforcementRoomCalculatorBase
-from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator.reinforcement_room_calculator_protocol import ReinforcementRoomCalculatorProtocol
+from koswat.dike.surroundings.point.point_obstacle_surroundings import (
+    PointObstacleSurroundings,
+)
+from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator.polderside_only_room_calculator import (
+    PoldersideOnlyRoomCalculator,
+)
+from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator.reinforcement_room_calculator_base import (
+    ReinforcementRoomCalculatorBase,
+)
+from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator.reinforcement_room_calculator_protocol import (
+    ReinforcementRoomCalculatorProtocol,
+)
+
 
 def get_reinforcement_and_obstacle_room_cases() -> list[pytest.param]:
     _polderside_width = 4
     _waterside_width = 2
     _base_calculator = PoldersideOnlyRoomCalculator(
-                required_polderside_width=_polderside_width
-            )
+        required_polderside_width=_polderside_width
+    )
     return [
         pytest.param(
             _base_calculator,
             PointObstacleSurroundings(inside_distance=_polderside_width),
             True,
-            id="exact_room"
+            id="exact_room",
         ),
         pytest.param(
             _base_calculator,
             PointObstacleSurroundings(inside_distance=_polderside_width + 1),
             True,
-            id="enough_room"
+            id="enough_room",
         ),
         pytest.param(
             _base_calculator,
-            PointObstacleSurroundings(inside_distance=_polderside_width + _waterside_width, outside_distance=0),
+            PointObstacleSurroundings(
+                inside_distance=_polderside_width + _waterside_width, outside_distance=0
+            ),
             True,
-            id="enough_room_only_at_polderside"
+            id="enough_room_only_at_polderside",
         ),
         pytest.param(
             _base_calculator,
-            PointObstacleSurroundings(outside_distance=_polderside_width + _waterside_width, inside_distance=0),
+            PointObstacleSurroundings(
+                outside_distance=_polderside_width + _waterside_width, inside_distance=0
+            ),
             False,
-            id="enough_room_only_at_waterside"
+            id="enough_room_only_at_waterside",
         ),
         pytest.param(
             _base_calculator,
-            PointObstacleSurroundings(inside_distance=_polderside_width - 1, outside_distance=_waterside_width - 1),
+            PointObstacleSurroundings(
+                inside_distance=_polderside_width - 1,
+                outside_distance=_waterside_width - 1,
+            ),
             False,
-            id="not_enough_room_at_either_side"
-        )
+            id="not_enough_room_at_either_side",
+        ),
     ]
 
+
 reinforcement_point_obstacle_cases = get_reinforcement_and_obstacle_room_cases()
+
 
 class TestPoldersideOnlyRoomCalculator:
 
     def test_when_initialize_then_returns_instance(self):
         # 1. When
-        _calculator = PoldersideOnlyRoomCalculator(
-            required_polderside_width=1.0
-        )
+        _calculator = PoldersideOnlyRoomCalculator(required_polderside_width=1.0)
 
         # 2. Then
         assert isinstance(_calculator, PoldersideOnlyRoomCalculator)
@@ -72,9 +90,15 @@ class TestPoldersideOnlyRoomCalculator:
         # 3. Then
         assert result == _polderside_width
 
-
-    @pytest.mark.parametrize("calculator, surroundings, expected_result", reinforcement_point_obstacle_cases)
-    def test_when_reinforcement_has_room_given_point_obstacle_surroundings_then_returns_expectation(self, calculator: PoldersideOnlyRoomCalculator, surroundings: PointObstacleSurroundings, expected_result: bool):
+    @pytest.mark.parametrize(
+        "calculator, surroundings, expected_result", reinforcement_point_obstacle_cases
+    )
+    def test_when_reinforcement_has_room_given_point_obstacle_surroundings_then_returns_expectation(
+        self,
+        calculator: PoldersideOnlyRoomCalculator,
+        surroundings: PointObstacleSurroundings,
+        expected_result: bool,
+    ):
         # 1. Given
         assert isinstance(calculator, PoldersideOnlyRoomCalculator)
         assert isinstance(surroundings, PointObstacleSurroundings)
@@ -84,3 +108,40 @@ class TestPoldersideOnlyRoomCalculator:
 
         # 3. Then
         assert _result == expected_result
+
+    @pytest.mark.parametrize(
+        "obstacle_distance, obstacle_buffer, is_location_safe",
+        [
+            pytest.param(
+                25, 4, True, id="Obstacle with buffer > distance, safe location"
+            ),
+            pytest.param(
+                20, 0, True, id="Obstacle without buffer > distance, safe location"
+            ),
+            pytest.param(
+                20, 2, True, id="Obstacle with buffer == distance, safe location"
+            ),
+            pytest.param(
+                20, 4, False, id="Obstacle with buffer < distance, unsafe location"
+            ),
+            pytest.param(
+                15, 0, False, id="Obstacle without buffer < distance, unsafe location"
+            ),
+        ],
+    )
+    def test_when_reinforcement_has_room_given_point_obstacle_surroundings_with_buffer_then_returns_expectation(
+        self, obstacle_distance: float, obstacle_buffer: float, is_location_safe: bool
+    ):
+        # 1. Given
+        _distance_to_check = 18
+
+        _calculator = PoldersideOnlyRoomCalculator(
+            required_polderside_width=_distance_to_check
+        )
+        _surroundings = PointObstacleSurroundings(inside_distance=obstacle_distance)
+
+        # 2. When
+        _result = _calculator.reinforcement_has_room(_surroundings, obstacle_buffer)
+
+        # 3. Then
+        assert _result == is_location_safe
