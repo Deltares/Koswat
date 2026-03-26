59a75e811e91efd58abc49fec11a77033a169824
ArdtK <ardt.klapwijk@deltares.nl>
2026-03-26 12:46:04 +0100

    chore: apply logic to new solution

--- koswat/dike_reinforcements/reinforcement_profile/reinforcement_room_calculator/polderside_only_room_calculator.py
+++ koswat/dike_reinforcements/reinforcement_profile/reinforcement_room_calculator/polderside_only_room_calculator.py
@@ -1,27 +1,33 @@
 """
-                    GNU GENERAL PUBLIC LICENSE
-                      Version 3, 29 June 2007
+                GNU GENERAL PUBLIC LICENSE
+                  Version 3, 29 June 2007
 
-    KOSWAT, from the dutch combination of words `Kosts-Wat` (what are the costs)
-    Copyright (C) 2025 Stichting Deltares
+KOSWAT, from the dutch combination of words `Kosts-Wat` (what are the costs)
+Copyright (C) 2025 Stichting Deltares
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from dataclasses import dataclass
-from koswat.dike.surroundings.point.point_obstacle_surroundings import PointObstacleSurroundings
-from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator import ReinforcementRoomCalculatorBase
+
+from koswat.dike.surroundings.point.point_obstacle_surroundings import (
+    PointObstacleSurroundings,
+)
+from koswat.dike_reinforcements.reinforcement_profile.reinforcement_room_calculator import (
+    ReinforcementRoomCalculatorBase,
+)
+
 
 @dataclass
 class PoldersideOnlyRoomCalculator(ReinforcementRoomCalculatorBase):
@@ -31,5 +37,11 @@ class PoldersideOnlyRoomCalculator(ReinforcementRoomCalculatorBase):
     def _required_width(self) -> float:
         return self.required_polderside_width
 
-    def reinforcement_has_room(self, point_obstacle_surroundings: PointObstacleSurroundings) -> bool:
-        return self._required_width_less_or_equal(point_obstacle_surroundings.inside_distance)
+    def reinforcement_has_room(
+        self,
+        point_obstacle_surroundings: PointObstacleSurroundings,
+        obstacle_buffer: float,
+    ) -> bool:
+        return self._required_width_less_or_equal(
+            point_obstacle_surroundings.inside_distance - obstacle_buffer
+        )
