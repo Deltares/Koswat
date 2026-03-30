09cb0d1f9d28f651b589da8e58cbc7af0730b436
ArdtK <ardt.klapwijk@deltares.nl>
2026-03-30 14:46:26 +0200

    chore: add reinforcement base class

---
+++ koswat/dike_reinforcements/input_profile/reinforcement_input_profile_base.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+from koswat.configuration.settings.koswat_general_settings import (
+    ConstructionTypeEnum,
+    SurtaxFactorEnum,
+)
+from koswat.dike.profile.koswat_input_profile_base import KoswatInputProfileBase
+from koswat.dike_reinforcements.input_profile.reinforcement_input_profile_protocol import (
+    ReinforcementInputProfileProtocol,
+)
+
+
+class ReinforcementInputProfileBase(
+    ABC, KoswatInputProfileBase, ReinforcementInputProfileProtocol
+):
+    active: bool = True
+    construction_length: float = 0.0
+    construction_type: ConstructionTypeEnum | None = None
+    soil_surtax_factor: SurtaxFactorEnum = SurtaxFactorEnum.NORMAAL
+    constructive_surtax_factor: SurtaxFactorEnum | None = None
+    land_purchase_surtax_factor: SurtaxFactorEnum | None = None
+    reinforcement_domain_name: str = ""
+
+    @property
+    def ground_price(self) -> float:
+        return self.ground_price_builtup
