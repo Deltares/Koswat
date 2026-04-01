21161b6eacdcc26ad3609852b31d5f8204b019eb
ArdtK <ardt.klapwijk@deltares.nl>
2026-04-01 14:20:58 +0200

    chore: adjust piping berm calculation

--- koswat/dike_reinforcements/reinforcement_profile/berm_calculator/berm_calculated_factors.py
+++ koswat/dike_reinforcements/reinforcement_profile/berm_calculator/berm_calculated_factors.py
@@ -1,22 +1,22 @@
 """
-                    GNU GENERAL PUBLIC LICENSE
-                      Version 3, 29 June 2007
+                GNU GENERAL PUBLIC LICENSE
+                  Version 3, 29 June 2007
 
-    KOSWAT, from the dutch combination of words `Kosts-Wat` (what are the costs)
-    Copyright (C) 2025 Stichting Deltares
+KOSWAT, from the dutch combination of words `Kosts-Wat` (what are the costs)
+Copyright (C) 2025 Stichting Deltares
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from __future__ import annotations
@@ -121,6 +121,9 @@ class BermCalculatedFactors:
             _dikebase_piping_old,
             _dikebase_height_new,
             _dikebase_stability_new,
+        )
+        _dikebase_piping_new_dict[InputProfileEnum.STABILITY_WALL_CREST] = (
+            _dikebase_piping_old
         )
         _dikebase_piping_new_dict[InputProfileEnum.VPS] = max(
             _dikebase_piping_old,
